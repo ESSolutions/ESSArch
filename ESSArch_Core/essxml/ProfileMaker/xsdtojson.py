f12c955d41af726d7f502e0e3ea69517dfad981f
Henrik Ek <henrik@essolutions.se>
2016-09-13 14:55:06 +0200

    comment out call for generateJsonRes function

--- ESSArch_Core/essxml/ProfileMaker/xsdtojson.py
+++ ESSArch_Core/essxml/ProfileMaker/xsdtojson.py
@@ -539,7 +539,7 @@ def generateJsonRes(schemaName, rootElement, namespace):
     # complexTypes = OrderedDict()
     # attributeGroups = OrderedDict()
 
-generateJsonRes("/Users/Axenu/Developer/ESSArch_Tools_Producer/ESSArch_TP2/esscore/template/templateGenerator/ead_geo.xsd", 'ead', 'ead')
+#generateJsonRes("/Users/Axenu/Developer/ESSArch_Tools_Producer/ESSArch_TP2/esscore/template/templateGenerator/ead_geo.xsd", 'ead', 'ead')
 # print generate()
 # print generate(2)
 # print generate(3)
