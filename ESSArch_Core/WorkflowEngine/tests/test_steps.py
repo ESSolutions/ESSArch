44b4779b39aa870f73c5667d3834449c0b1c080f
Oskar Persson <oskar.persson@polken.se>
2016-09-26 08:34:05 +0200

    Add tests for parallel steps

--- ESSArch_Core/WorkflowEngine/tests/test_steps.py
+++ ESSArch_Core/WorkflowEngine/tests/test_steps.py
@@ -354,3 +354,280 @@ class test_running_steps(TestCase):
         )
 
         self.assertTrue(first_task.exists())
+
+    def test_parallel_step(self):
+        t1_val = 123
+        t2_val = 456
+        t3_val = 789
+
+        step = ProcessStep.objects.create(
+            name="Test",
+        )
+
+        t1 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.First",
+            params={
+                "foo": t1_val,
+            },
+        )
+
+        t2 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.Second",
+            params={
+                "foo": t2_val,
+            },
+        )
+
+        t3 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.Third",
+            params={
+                "foo": t3_val,
+            },
+        )
+
+        step.tasks = [t1, t2, t3]
+        step.parallel = True
+        step.save()
+
+        step.run().get()
+        self.assertEqual(step.status, celery_states.SUCCESS)
+
+        t1.refresh_from_db()
+        t2.refresh_from_db()
+        t3.refresh_from_db()
+
+        self.assertEqual(t1.status, celery_states.SUCCESS)
+        self.assertEqual(t2.status, celery_states.SUCCESS)
+        self.assertEqual(t3.status, celery_states.SUCCESS)
+
+    def test_undo_parallel_step(self):
+        t1_val = 123
+        t2_val = os.path.join(self.test_dir, "foo.txt")
+        t3_val = 789
+
+        step = ProcessStep.objects.create(
+            name="Test",
+        )
+
+        t1 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.First",
+            params={
+                "foo": t1_val,
+            },
+        )
+
+        t2 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.FailIfFileNotExists",
+            params={
+                "filename": t2_val,
+            },
+        )
+
+        t3 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.Third",
+            params={
+                "foo": t3_val,
+            },
+        )
+
+        step.tasks = [t1, t2, t3]
+        step.parallel = True
+        step.save()
+
+        with self.assertRaises(AssertionError):
+            step.run().get()
+
+        self.assertEqual(step.status, celery_states.FAILURE)
+
+        step.undo().get()
+
+        t1.refresh_from_db()
+        t2.refresh_from_db()
+        t3.refresh_from_db()
+
+        self.assertEqual(t1.status, celery_states.SUCCESS)
+        self.assertEqual(t2.status, celery_states.FAILURE)
+        self.assertEqual(t3.status, celery_states.SUCCESS)
+
+        self.assertTrue(t1.undone)
+        self.assertTrue(t2.undone)
+        self.assertTrue(t3.undone)
+
+    def test_undo_only_failed_parallel_step(self):
+        t1_val = 123
+        t2_val = os.path.join(self.test_dir, "foo.txt")
+        t3_val = 789
+
+        step = ProcessStep.objects.create(
+            name="Test",
+        )
+
+        t1 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.First",
+            params={
+                "foo": t1_val,
+            },
+        )
+
+        t2 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.FailIfFileNotExists",
+            params={
+                "filename": t2_val,
+            },
+        )
+
+        t3 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.Third",
+            params={
+                "foo": t3_val,
+            },
+        )
+
+        step.tasks = [t1, t2, t3]
+        step.parallel = True
+        step.save()
+
+        with self.assertRaises(AssertionError):
+            step.run().get()
+
+        self.assertEqual(step.status, celery_states.FAILURE)
+
+        step.undo(only_failed=True).get()
+
+        t1.refresh_from_db()
+        t2.refresh_from_db()
+        t3.refresh_from_db()
+
+        self.assertEqual(t1.status, celery_states.SUCCESS)
+        self.assertEqual(t2.status, celery_states.FAILURE)
+        self.assertEqual(t3.status, celery_states.SUCCESS)
+
+        self.assertFalse(t1.undone)
+        self.assertTrue(t2.undone)
+        self.assertFalse(t3.undone)
+
+    def test_retry_parallel_step(self):
+        t1_val = 123
+        t2_val = os.path.join(self.test_dir, "foo.txt")
+        t3_val = 789
+
+        step = ProcessStep.objects.create(
+            name="Test",
+        )
+
+        t1 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.First",
+            params={
+                "foo": t1_val,
+            },
+        )
+
+        t2 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.FailIfFileNotExists",
+            params={
+                "filename": t2_val,
+            },
+        )
+
+        t3 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.Third",
+            params={
+                "foo": t3_val,
+            },
+        )
+
+        step.tasks = [t1, t2, t3]
+        step.parallel = True
+        step.save()
+
+        with self.assertRaises(AssertionError):
+            step.run().get()
+
+        self.assertEqual(step.status, celery_states.FAILURE)
+
+        step.undo().get()
+
+        open(t2_val, 'a').close()
+
+        step.retry().get()
+
+        self.assertEqual(step.status, celery_states.SUCCESS)
+
+        t1.refresh_from_db()
+        t2.refresh_from_db()
+        t3.refresh_from_db()
+
+        self.assertEqual(t1.status, celery_states.SUCCESS)
+        self.assertEqual(t2.status, celery_states.FAILURE)
+        self.assertEqual(t3.status, celery_states.SUCCESS)
+
+        self.assertTrue(t1.undone)
+        self.assertTrue(t2.undone)
+        self.assertTrue(t3.undone)
+
+        self.assertTrue(t1.retried)
+        self.assertTrue(t2.retried)
+        self.assertTrue(t3.retried)
+
+    def test_retry_only_failed_parallel_step(self):
+        t1_val = 123
+        t2_val = os.path.join(self.test_dir, "foo.txt")
+        t3_val = 789
+
+        step = ProcessStep.objects.create(
+            name="Test",
+        )
+
+        t1 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.First",
+            params={
+                "foo": t1_val,
+            },
+        )
+
+        t2 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.FailIfFileNotExists",
+            params={
+                "filename": t2_val,
+            },
+        )
+
+        t3 = ProcessTask.objects.create(
+            name="preingest.tests.tasks.Third",
+            params={
+                "foo": t3_val,
+            },
+        )
+
+        step.tasks = [t1, t2, t3]
+        step.parallel = True
+        step.save()
+
+        with self.assertRaises(AssertionError):
+            step.run().get()
+
+        self.assertEqual(step.status, celery_states.FAILURE)
+
+        step.undo(only_failed=True).get()
+
+        open(t2_val, 'a').close()
+
+        step.retry().get()
+
+        self.assertEqual(step.status, celery_states.SUCCESS)
+
+        t1.refresh_from_db()
+        t2.refresh_from_db()
+        t3.refresh_from_db()
+
+        self.assertEqual(t1.status, celery_states.SUCCESS)
+        self.assertEqual(t2.status, celery_states.FAILURE)
+        self.assertEqual(t3.status, celery_states.SUCCESS)
+
+        self.assertFalse(t1.undone)
+        self.assertTrue(t2.undone)
+        self.assertFalse(t3.undone)
+
+        self.assertFalse(t1.retried)
+        self.assertTrue(t2.retried)
+        self.assertFalse(t3.retried)
