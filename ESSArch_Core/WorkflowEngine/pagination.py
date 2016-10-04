ff3506fae788bbf58f824b4b2ebc82f783e2dc6c
Oskar Persson <oskar.persson@polken.se>
2016-10-04 15:24:31 +0200

    Add pagination support to backend

---
+++ ESSArch_Core/WorkflowEngine/pagination.py
@@ -0,0 +1,23 @@
+from rest_framework import pagination
+from rest_framework.response import Response
+
+class LinkHeaderPagination(pagination.PageNumberPagination):
+    page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        next_url = self.get_next_link()
+        previous_url = self.get_previous_link()
+
+        if next_url is not None and previous_url is not None:
+            link = '<{next_url}>; rel="next", <{previous_url}>; rel="prev"'
+        elif next_url is not None:
+            link = '<{next_url}>; rel="next"'
+        elif previous_url is not None:
+            link = '<{previous_url}>; rel="prev"'
+        else:
+            link = ''
+
+        link = link.format(next_url=next_url, previous_url=previous_url)
+        headers = {'Link': link, 'Count': self.page.paginator.count} if link else {}
+
+        return Response(data, headers=headers)
