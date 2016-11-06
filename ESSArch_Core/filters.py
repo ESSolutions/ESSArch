2ae80bb3c7af3aed8dc8c00ca4e3535a591621cb
Oskar Persson <oskar.persson@polken.se>
2016-11-06 14:20:30 +0100

    Add support for filtering with comma-separated values

---
+++ ESSArch_Core/filters.py
@@ -0,0 +1,8 @@
+import django_filters
+
+from django_filters.fields import Lookup
+
+class ListFilter(django_filters.Filter):
+    def filter(self, qs, value):
+        value_list = value.split(u',')
+        return super(ListFilter, self).filter(qs, Lookup(value_list, 'in'))
