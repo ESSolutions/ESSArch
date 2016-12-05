1870d8b1915150f06b3677dc1f7f4b713d69446c
Oskar Persson <oskar.persson@polken.se>
2016-12-05 15:37:43 +0100

    Add mkdir_p method to utils

--- ESSArch_Core/util.py
+++ ESSArch_Core/util.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-import hashlib, os, platform, pyclbr, re
+import errno, hashlib, os, platform, pyclbr, re
 
 from django.utils.timezone import get_current_timezone
 
@@ -246,3 +246,17 @@ def alg_from_str(algname):
         return valid[algname]
     except:
         raise KeyError("Algorithm %s does not exist" % algname)
+
+
+def mkdir_p(path):
+    """
+    http://stackoverflow.com/a/600612/1523238
+    """
+
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
