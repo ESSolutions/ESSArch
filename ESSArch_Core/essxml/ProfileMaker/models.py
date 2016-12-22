d3eeae3ff2889ea7faed5302de00d8c42e60d250
Oskar Persson <oskar.persson@polken.se>
2016-12-23 00:28:59 +0100

    Fix E251 pep8 warning

--- ESSArch_Core/essxml/ProfileMaker/models.py
+++ ESSArch_Core/essxml/ProfileMaker/models.py
@@ -19,7 +19,7 @@ class templatePackage(models.Model):
     # treeData = jsonfield.JSONField(null=True)
     allElements = jsonfield.JSONField(null=True)
     # isTreeCreated = models.BooleanField(default=True)
-    name = models.CharField(max_length = 255, primary_key=True)
+    name = models.CharField(max_length=255, primary_key=True)
     root_element = models.CharField(max_length=55, default='')
     extensions = models.ManyToManyField(extensionPackage)
 
