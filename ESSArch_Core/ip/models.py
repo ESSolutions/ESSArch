4d1bf655623b03f1481beb93bcef068565c756a1
Oskar Persson <oskar.persson@polken.se>
2017-01-30 12:57:22 +0100

    Remove old licenses

--- ESSArch_Core/ip/models.py
+++ ESSArch_Core/ip/models.py
@@ -22,29 +22,6 @@
     Email - essarch@essolutions.se
 """
 
-"""
-    ESSArch Tools - ESSArch is an Electronic Preservation Platform
-    Copyright (C) 2005-2016  ES Solutions AB
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Contact information:
-    Web - http://www.essolutions.se
-    Email - essarch@essolutions.se
-"""
-
-# Create your models here.
 from __future__ import division
 
 from celery import states as celery_states
