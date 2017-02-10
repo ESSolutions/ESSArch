8e3bafa469db1a82def3829b96487cbe0dc5e4d3
Oskar Persson <oskar.persson@polken.se>
2017-02-10 14:16:15 +0100

    Pass CalculateChecksumTestCase

--- ESSArch_Core/WorkflowEngine/tests/test_real_tasks.py
+++ ESSArch_Core/WorkflowEngine/tests/test_real_tasks.py
@@ -97,7 +97,7 @@ class CalculateChecksumTestCase(TestCase):
         )
 
         expected = "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
-        actual = task.run().get().get(task.pk)
+        actual = task.run().get()
 
         self.assertEqual(expected, actual)
 
@@ -112,7 +112,7 @@ class CalculateChecksumTestCase(TestCase):
         )
 
         expected = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-        actual = task.run().get().get(task.pk)
+        actual = task.run().get()
 
         self.assertEqual(expected, actual)
 
@@ -129,7 +129,7 @@ class CalculateChecksumTestCase(TestCase):
         )
 
         expected = "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
-        actual = task.run().get().get(task.pk)
+        actual = task.run().get()
 
         self.assertEqual(expected, actual)
 
@@ -146,7 +146,7 @@ class CalculateChecksumTestCase(TestCase):
         )
 
         expected = "acbd18db4cc2f85cedef654fccc4a4d8"
-        actual = task.run().get().get(task.pk)
+        actual = task.run().get()
 
         self.assertEqual(expected, actual)
 
@@ -163,7 +163,7 @@ class CalculateChecksumTestCase(TestCase):
         )
 
         expected = "0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33"
-        actual = task.run().get().get(task.pk)
+        actual = task.run().get()
 
         self.assertEqual(expected, actual)
 
@@ -180,7 +180,7 @@ class CalculateChecksumTestCase(TestCase):
         )
 
         expected = "0808f64e60d58979fcb676c96ec938270dea42445aeefcd3a4e6f8db"
-        actual = task.run().get().get(task.pk)
+        actual = task.run().get()
 
         self.assertEqual(expected, actual)
 
@@ -197,7 +197,7 @@ class CalculateChecksumTestCase(TestCase):
         )
 
         expected = "98c11ffdfdd540676b1a137cb1a22b2a70350c9a44171d6b1180c6be5cbb2ee3f79d532c8a1dd9ef2e8e08e752a3babb"
-        actual = task.run().get().get(task.pk)
+        actual = task.run().get()
 
         self.assertEqual(expected, actual)
 
@@ -214,7 +214,7 @@ class CalculateChecksumTestCase(TestCase):
         )
 
         expected = "f7fbba6e0636f890e56fbbf3283e524c6fa3204ae298382d624741d0dc6638326e282c41be5e4254d8820772c5518a2c5a8c0c7f7eda19594a7eb539453e1ed7"
-        actual = task.run().get().get(task.pk)
+        actual = task.run().get()
 
         self.assertEqual(expected, actual)
 
