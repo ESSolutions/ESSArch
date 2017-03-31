e30453c50fabac334250014b943b809dc565ee3c
Oskar Persson <oskar.persson@polken.se>
2017-03-31 12:30:41 +0200

    Fix order of IP migrations

--- ESSArch_Core/ip/migrations/0030_auto_20170328_1545.py
+++ ESSArch_Core/ip/migrations/0030_auto_20170328_1545.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ip', '0029_auto_20170223_1116'),
+        ('ip', '0033_auto_20170306_1440'),
     ]
 
     operations = [
