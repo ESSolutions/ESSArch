ee712b55c2177118b468c2ac9a25e9c8339d8aa4
Oskar Persson <oskar.persson@polken.se>
2017-04-16 23:12:43 +0200

    Allow tape_slot field in storage medium to be null

--- ESSArch_Core/storage/models.py
+++ ESSArch_Core/storage/models.py
@@ -213,7 +213,7 @@ class StorageMedium(models.Model):
 
     agent = models.ForeignKey('auth.User', on_delete=models.PROTECT)
     storage_target = models.ForeignKey('StorageTarget')
-    tape_slot = models.OneToOneField('TapeSlot', models.PROTECT, related_name='storage_medium')
+    tape_slot = models.OneToOneField('TapeSlot', models.PROTECT, related_name='storage_medium', null=True)
     tape_drive = models.OneToOneField('TapeDrive', models.PROTECT, related_name='storage_medium', null=True)
 
     class Meta:
