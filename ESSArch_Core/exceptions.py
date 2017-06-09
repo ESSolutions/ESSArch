3fa30a6e943c31d006f425e973a9ec4ba75eccaa
Oskar Persson <oskar.persson@polken.se>
2017-06-09 15:39:20 +0200

    Add Conflict exception

--- ESSArch_Core/exceptions.py
+++ ESSArch_Core/exceptions.py
@@ -1,2 +1,12 @@
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
+
 class FileFormatNotAllowed(Exception):
     pass
+
+
+class Conflict(APIException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = 'The request could not be completed due to a conflict with the target resource'
+    default_code = 'conflict'
