7fd95f84d755f6b2796b3a0f81fb9860661fc852
Oskar Persson <oskar.persson@polken.se>
2017-06-15 12:09:56 +0200

    Add files method to IP model

--- ESSArch_Core/ip/models.py
+++ ESSArch_Core/ip/models.py
@@ -24,9 +24,24 @@
 
 from __future__ import division
 
+import datetime
+import mimetypes
+import os
+import re
+import tarfile
+import zipfile
+
+from operator import itemgetter
+
 from celery import states as celery_states
 
 from django.db import models
+from django.http.response import HttpResponse
+
+from rest_framework import exceptions, filters, permissions, status
+from rest_framework.response import Response
+
+from ESSArch_Core.configuration.models import Path
 
 from ESSArch_Core.profiles.models import (
     SubmissionAgreement as SA,
@@ -34,7 +49,10 @@ from ESSArch_Core.profiles.models import (
 )
 
 from ESSArch_Core.util import (
+    get_files_and_dirs,
     get_tree_size_and_count,
+    in_directory,
+    timestamp_to_datetime,
 )
 
 import math
@@ -380,6 +398,140 @@ class InformationPackage(models.Model):
                     return 0
 
             return 0
+
+    def files(self, path=''):
+        mimetypes.suffix_map = {}
+        mimetypes.encodings_map = {}
+        mimetypes.types_map = {}
+        mimetypes.common_types = {}
+        mimetypes_file = Path.objects.get(
+            entity="path_mimetypes_definitionfile"
+        ).value
+        mimetypes.init(files=[mimetypes_file])
+        mtypes = mimetypes.types_map
+
+        if os.path.isfile(self.object_path):
+            container = self.object_path
+            xml = os.path.splitext(self.object_path)[0] + '.xml'
+
+            if path.startswith(os.path.basename(container)):
+                fullpath = os.path.join(os.path.dirname(container), path)
+
+                if tarfile.is_tarfile(container):
+                    with tarfile.open(container) as tar:
+                        if fullpath == container:
+                            entries = []
+                            for member in tar.getmembers():
+                                if not member.isfile():
+                                    continue
+
+                                entries.append({
+                                    "name": member.name,
+                                    "type": 'file',
+                                    "size": member.size,
+                                    "modified": timestamp_to_datetime(member.mtime),
+                                })
+                            return Response(entries)
+                        else:
+                            subpath = fullpath[len(container)+1:]
+                            try:
+                                member = tar.getmember(subpath)
+
+                                if not member.isfile():
+                                    raise exceptions.NotFound
+
+                                f = tar.extractfile(member)
+                                content_type = mtypes.get(os.path.splitext(subpath)[1])
+                                return HttpResponse(f.read(), content_type=content_type)
+                            except KeyError:
+                                raise exceptions.NotFound
+
+                elif zipfile.is_zipfile(container):
+                    with zipfile.ZipFile(container) as zipf:
+                        if fullpath == container:
+                            entries = []
+                            for member in zipf.filelist:
+                                if member.filename.endswith('/'):
+                                    continue
+
+                                entries.append({
+                                    "name": member.filename,
+                                    "type": 'file',
+                                    "size": member.file_size,
+                                    "modified": datetime.datetime(*member.date_time),
+                                })
+                            return Response(entries)
+                        else:
+                            subpath = fullpath[len(container)+1:]
+                            try:
+                                f = zipf.open(subpath)
+                                content_type = mtypes.get(os.path.splitext(subpath)[1])
+                                return HttpResponse(f.read(), content_type=content_type)
+                            except KeyError:
+                                raise exceptions.NotFound
+
+
+                content_type = mtypes.get(os.path.splitext(fullpath)[1])
+                return HttpResponse(open(fullpath).read(), content_type=content_type)
+            elif os.path.isfile(xml) and path == os.path.basename(xml):
+                fullpath = os.path.join(os.path.dirname(container), path)
+                content_type = mtypes.get(os.path.splitext(fullpath)[1])
+                return HttpResponse(open(fullpath).read(), content_type=content_type)
+            elif path == '':
+                entries = []
+
+                entries.append({
+                    "name": os.path.basename(container),
+                    "type": 'file',
+                    "size": os.path.getsize(container),
+                    "modified": timestamp_to_datetime(os.path.getmtime(container)),
+                })
+
+                if os.path.isfile(xml):
+                    entries.append({
+                        "name": os.path.basename(xml),
+                        "type": 'file',
+                        "size": os.path.getsize(xml),
+                        "modified": timestamp_to_datetime(os.path.getmtime(xml)),
+                    })
+                return Response(entries)
+
+            elif path is not None:
+                raise exceptions.NotFound
+
+        entries = []
+        fullpath = os.path.join(self.object_path, path)
+
+        if not in_directory(fullpath, self.object_path):
+            raise exceptions.ParseError('Illegal path %s' % path)
+
+        if not os.path.exists(fullpath):
+            raise exceptions.NotFound
+
+        if os.path.isfile(fullpath):
+            content_type = mtypes.get(os.path.splitext(fullpath)[1])
+            return HttpResponse(open(fullpath).read(), content_type=content_type)
+
+        for entry in get_files_and_dirs(fullpath):
+            entry_type = "dir" if entry.is_dir() else "file"
+
+            if entry_type == 'file' and re.search(r'\_\d+$', entry.name) is not None:  # file chunk
+                continue
+
+            size, _ = get_tree_size_and_count(entry.path)
+
+            entries.append(
+                {
+                    "name": os.path.basename(entry.path),
+                    "type": entry_type,
+                    "size": size,
+                    "modified": timestamp_to_datetime(entry.stat().st_mtime),
+                }
+            )
+
+        sorted_entries = sorted(entries, key=itemgetter('name'))
+        return Response(sorted_entries)
+
 
     def delete(self, *args, **kwargs):
         super(InformationPackage, self).delete(*args, **kwargs)
