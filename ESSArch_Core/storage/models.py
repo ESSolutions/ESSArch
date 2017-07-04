8baaa479f441e715e2ca51046df85866f0621f62
Oskar Persson <oskar.persson@polken.se>
2017-07-04 17:35:37 +0200

    Convert id field to uuid, add drive_id to TapeDrive

--- ESSArch_Core/storage/models.py
+++ ESSArch_Core/storage/models.py
@@ -337,7 +337,8 @@ class StorageObject(models.Model):
 
 
 class TapeDrive(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    drive_id = models.IntegerField()
     device = models.CharField(max_length=255, unique=True)
     io_queue_entry = models.OneToOneField('IOQueue', models.PROTECT, related_name='tape_drive', null=True)
     num_of_mounts = models.IntegerField(default=0)
