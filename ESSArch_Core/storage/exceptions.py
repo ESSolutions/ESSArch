aeb915f8b58959e41e3e299d731612326cd7b122
Oskar Persson <oskar.persson@polken.se>
2017-07-10 16:09:26 +0200

    Add storage exceptions from EPP

--- ESSArch_Core/storage/exceptions.py
+++ ESSArch_Core/storage/exceptions.py
@@ -20,3 +20,23 @@ class MTInvalidOperationOrDeviceNameException(Exception):
 
 class MTFailedOperationException(Exception):
     pass
+
+
+class TapeDriveLockedError(Exception):
+    """The tape drive is locked"""
+    pass
+
+
+class TapeMountedAndLockedByOtherError(Exception):
+    """The tape is mounted and locked by another process"""
+    pass
+
+
+class TapeMountedError(Exception):
+    """The tape is already mounted"""
+    pass
+
+
+class TapeUnmountedError(Exception):
+    """The tape is already unmounted"""
+    pass
