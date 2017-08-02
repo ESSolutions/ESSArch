168d0f7f4e24c91ad49c85f37d89b2ada8a657e3
Oskar Persson <oskar.persson@polken.se>
2017-08-02 11:14:13 +0000

    Add remaining hashlib algorithms to ArchivePolicy choices

--- ESSArch_Core/configuration/models.py
+++ ESSArch_Core/configuration/models.py
@@ -138,7 +138,11 @@ class ArchivePolicy(models.Model):
 
     CHECKSUM_ALGORITHM_CHOICES = (
         (MD5, 'md5'),
+        (SHA1, 'sha-1'),
+        (SHA224, 'sha-224'),
         (SHA256, 'sha-256'),
+        (SHA384, 'sha-384'),
+        (SHA512, 'sha-512'),
     )
 
     IP_TYPE_CHOICES = (
