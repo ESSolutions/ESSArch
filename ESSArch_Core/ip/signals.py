f3053f093125731f3ceea1fb5542697fa257e4e3
Oskar Persson <oskar.persson@polken.se>
2017-08-17 14:06:30 +0200

    Delete tar if it exists on workarea removal

--- ESSArch_Core/ip/signals.py
+++ ESSArch_Core/ip/signals.py
@@ -1,4 +1,5 @@
 import errno
+import os
 import shutil
 
 from django.db.models.signals import post_delete
@@ -10,6 +11,12 @@ from ESSArch_Core.ip.models import Workarea
 def workarea_post_delete(sender, instance, using, **kwargs):
     try:
         shutil.rmtree(instance.path)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+
+    try:
+        os.remove(instance.path + '.tar')
     except OSError as e:
         if e.errno != errno.ENOENT:
             raise
