da8eaf9a465850bba2bc91cdb4b279024195bf35
Oskar Persson <oskar.persson@polken.se>
2017-08-19 00:21:05 +0200

    Add support for ADFS authentication

---
+++ ESSArch_Core/auth/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import url
+
+from rest_auth.views import (
+    UserDetailsView, PasswordChangeView,
+    PasswordResetView, PasswordResetConfirmView
+)
+
+from ESSArch_Core.auth.views import (
+    LoginView, LogoutView,
+)
+
+urlpatterns = [
+    # URLs that do not require a session or valid token
+    url(r'^password/reset/$', PasswordResetView.as_view(),
+        name='rest_password_reset'),
+    url(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(),
+        name='rest_password_reset_confirm'),
+    url(r'^login/$', LoginView.as_view(), name='rest_login'),
+    url(r'^logout/$', LogoutView.as_view(), name='rest_logout'),
+    url(r'^user/$', UserDetailsView.as_view(), name='rest_user_details'),
+    url(r'^password/change/$', PasswordChangeView.as_view(),
+        name='rest_password_change'),
+]
--- ESSArch_Core/auth/views.py
+++ ESSArch_Core/auth/views.py
@@ -71,3 +71,144 @@ class PermissionViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = Permission.objects.all()
     serializer_class = PermissionSerializer
+
+from django.conf import settings
+
+from django.contrib.auth import (
+    authenticate,
+    login as django_login,
+    logout as django_logout
+)
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect, reverse
+from django.utils.translation import ugettext_lazy as _
+
+from djangosaml2.views import logout as saml2_logout
+
+from rest_framework import exceptions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.generics import RetrieveUpdateAPIView
+
+from allauth.account import app_settings as allauth_settings
+
+from rest_auth.app_settings import (
+    TokenSerializer, UserDetailsSerializer, LoginSerializer,
+    PasswordResetSerializer, PasswordResetConfirmSerializer,
+    PasswordChangeSerializer, JWTSerializer, create_token
+)
+from rest_auth.models import TokenModel
+
+from rest_auth.utils import jwt_encode
+
+class LoginView(GenericAPIView):
+
+    """
+    Check the credentials and return the REST Token
+    if the credentials are valid and authenticated.
+    Calls Django Auth login method to register User ID
+    in Django session framework
+
+    Accept the following POST parameters: username, password
+    Return the REST Framework Token Object's key.
+    """
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
+    token_model = TokenModel
+
+    def process_login(self):
+        django_login(self.request, self.user)
+
+    def get_response_serializer(self):
+        if getattr(settings, 'REST_USE_JWT', False):
+            response_serializer = JWTSerializer
+        else:
+            response_serializer = TokenSerializer
+        return response_serializer
+
+    def login(self):
+        print 'login...'
+        self.user = self.serializer.validated_data['user']
+
+        if getattr(settings, 'REST_USE_JWT', False):
+            self.token = jwt_encode(self.user)
+        else:
+            self.token = create_token(self.token_model, self.user, self.serializer)
+
+        if getattr(settings, 'REST_SESSION_LOGIN', True):
+            self.process_login()
+
+    def get_response(self):
+        serializer_class = self.get_response_serializer()
+
+        if getattr(settings, 'REST_USE_JWT', False):
+            data = {
+                'user': self.user,
+                'token': self.token
+            }
+            serializer = serializer_class(instance=data, context={'request': self.request})
+        else:
+            serializer = serializer_class(instance=self.token, context={'request': self.request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        self.request = request
+        try:
+		self.serializer = self.get_serializer(data=self.request.data)
+		self.serializer.is_valid(raise_exception=True)
+        	self.login()
+	except:
+		data = self.request.data
+		user = authenticate(username=data['username'], password=data['password'])
+		print user
+
+		if user is None:
+		    raise exceptions.ParseError('Invalid username or password')
+
+        	django_login(self.request, user)
+
+        return self.get_response()
+
+
+class LogoutView(APIView):
+
+    """
+    Calls Django logout method and delete the Token object
+    assigned to the current User object.
+
+    Accepts/Returns nothing.
+    """
+    permission_classes = (AllowAny,)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            if allauth_settings.LOGOUT_ON_GET:
+                response = self.logout(request)
+            else:
+                response = self.http_method_not_allowed(request, *args, **kwargs)
+        except Exception as exc:
+            response = self.handle_exception(exc)
+
+        return self.finalize_response(request, response, *args, **kwargs)
+
+    def post(self, request):
+        if getattr(settings, 'ENABLE_ADFS_LOGIN', False):
+            return Response({'redirect': reverse('saml2:saml2_logout')})
+
+        return self.logout(request)
+
+    def logout(self, request):
+        try:
+            request.user.auth_token.delete()
+        except (AttributeError, ObjectDoesNotExist):
+            pass
+
+        django_logout(request)
+
+
+        return Response({"success": _("Successfully logged out.")},
+                        status=status.HTTP_200_OK)
