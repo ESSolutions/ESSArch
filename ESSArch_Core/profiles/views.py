7f3f2a3d1c0b9366929db397554dcb6c8dc83c0c
Oskar Persson <oskar.persson@polken.se>
2017-09-08 16:44:03 +0200

    Create ProfileIPDataViewSet

--- ESSArch_Core/profiles/views.py
+++ ESSArch_Core/profiles/views.py
@@ -2,11 +2,12 @@ from rest_framework import exceptions
 
 from ESSArch_Core.profiles.serializers import (
     ProfileIPSerializer,
+    ProfileIPDataSerializer,
     ProfileIPWriteSerializer,
 )
 
 from ESSArch_Core.profiles.models import (
-    ProfileIP,
+    ProfileIP, ProfileIPData,
 )
 
 from django_filters.rest_framework import DjangoFilterBackend
@@ -34,3 +35,16 @@ class ProfileIPViewSet(viewsets.ModelViewSet):
 
         return super(ProfileIPViewSet, self).update(request, *args, **kwargs)
 
+
+class ProfileIPDataViewSet(viewsets.ModelViewSet):
+    queryset = ProfileIPData.objects.all()
+    serializer_class = ProfileIPDataSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.relation.LockedBy is not None:
+            detail = 'Method "{method}" is not allowed on locked profiles'.format(method=request.method)
+            raise exceptions.MethodNotAllowed(method=request.method, detail=detail)
+
+        return super(ProfileIPDataViewSet, self).update(request, *args, **kwargs)
