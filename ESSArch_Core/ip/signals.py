59ffc3a95420838445083e0511278eadd1a99ad2
Oskar Persson <oskar.persson@polken.se>
2017-09-13 12:40:47 +0200

    Log deletion of IP

--- ESSArch_Core/ip/signals.py
+++ ESSArch_Core/ip/signals.py
@@ -1,10 +1,20 @@
 import errno
+import logging
 import os
 import shutil
 
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from ESSArch_Core.ip.models import Workarea
+
+from ESSArch_Core.ip.models import InformationPackage, Workarea
+
+
+logger = logging.getLogger('essarch.core')
+
+
+@receiver(post_delete, sender=InformationPackage)
+def ip_post_delete(sender, instance, using, **kwargs):
+    logger.info('Information package %s was deleted' % instance.pk)
 
 
 @receiver(post_delete, sender=Workarea)
