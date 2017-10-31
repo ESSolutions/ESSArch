f354cf850134f7c8f0c20b22e9a640a707cd60c6
Oskar Persson <oskar.persson@polken.se>
2017-10-31 15:49:25 +0100

    Add PaginatedViewMixin

---
+++ ESSArch_Core/mixins.py
@@ -0,0 +1,31 @@
+from rest_framework.settings import api_settings
+
+class PaginatedViewMixin(object):
+    pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
+
+    @property
+    def paginator(self):
+        """
+        The paginator instance associated with the view, or `None`.
+        """
+        if not hasattr(self, '_paginator'):
+            if self.pagination_class is None:
+                self._paginator = None
+            else:
+                self._paginator = self.pagination_class()
+        return self._paginator
+
+    def paginate_queryset(self, queryset):
+        """
+        Return a single page of results, or `None` if pagination is disabled.
+        """
+        if self.paginator is None:
+            return None
+        return self.paginator.paginate_queryset(queryset, self.request, view=self)
+
+    def get_paginated_response(self, data):
+        """
+        Return a paginated style `Response` object for the given output data.
+        """
+        assert self.paginator is not None
+        return self.paginator.get_paginated_response(data)
