a7784f81c1f533487186638ff961cbd307f0fe6c
Oskar Persson <oskar.persson@polken.se>
2017-11-13 00:33:01 +0100

    Fix EventIP migration for sqlite and MS SQL Server

--- ESSArch_Core/ip/migrations/0051_auto_20171002_2137.py
+++ ESSArch_Core/ip/migrations/0051_auto_20171002_2137.py
@@ -3,17 +3,29 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import django.db.models.deletion
 import uuid
 
 
 def forwards_func(apps, schema_editor):
     EventIP = apps.get_model("ip", "EventIP")
+    TempEventIP = apps.get_model("ip", "TempEventIP")
     db_alias = schema_editor.connection.alias
 
-    i = 1
     for e in EventIP.objects.using(db_alias).all().order_by('eventDateTime').iterator():
-        EventIP.objects.using(db_alias).filter(eventIdentifierValue=e.eventIdentifierValue).update(id=i)
-        i += 1
+        tmp = TempEventIP.objects.using(db_alias).create(
+            eventIdentifierValue=str(e.id),
+            eventType=e.eventType,
+            task=e.task,
+            application=e.application,
+            eventVersion=e.eventVersion,
+            eventOutcome=e.eventOutcome,
+            eventOutcomeDetailNote=e.eventOutcomeDetailNote,
+            linkingAgentIdentifierValue=e.linkingAgentIdentifierValue,
+            linkingObjectIdentifierValue=e.linkingObjectIdentifierValue,
+        )
+        tmp.eventDateTime = e.eventDateTime
+        tmp.save(update_fields=['eventDateTime'])
 
 
 def reverse_func(apps, schema_editor):
@@ -21,36 +33,33 @@ def reverse_func(apps, schema_editor):
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('ip', '0050_auto_20170925_1602'),
     ]
 
     operations = [
-        migrations.RenameField(
-            model_name='eventip',
-            old_name='id',
-            new_name='eventIdentifierValue',
-        ),
-        migrations.AlterField(
-            model_name='eventip',
-            name='eventIdentifierValue',
-            field=models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False),
-        ),
-        migrations.AddField(
-            model_name='eventip',
-            name='id',
-            field=models.IntegerField(default=0),
+        migrations.AlterModelTable('EventIP', 'ip_tempeventip'),
+        migrations.CreateModel(
+            name='TempEventIP',
+            fields=[
+                ('id', models.BigAutoField(primary_key=True, serialize=False)),
+                ('eventIdentifierValue', models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)),
+                ('eventType', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='configuration.EventType')),
+                ('eventDateTime', models.DateTimeField(auto_now_add=True)),
+                ('task', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='events', to='WorkflowEngine.ProcessTask')),
+                ('application', models.CharField(max_length=255)),
+                ('eventVersion', models.CharField(max_length=255)),
+                ('eventOutcome', models.IntegerField(choices=[(0, b'Success'), (1, b'Failure')], default=None, null=True)),
+                ('eventOutcomeDetailNote', models.CharField(max_length=1024)),
+                ('linkingAgentIdentifierValue', models.CharField(max_length=255, blank=True)),
+                ('linkingObjectIdentifierValue', models.CharField(max_length=255, blank=True)),
+            ],
+            options={
+                'db_table': 'ip_eventip',
+            },
         ),
         migrations.RunPython(forwards_func, reverse_func),
-        migrations.AlterField(
-            model_name='eventip',
-            name='id',
-            field=models.IntegerField(primary_key=True),
-        ),
-        migrations.AlterField(
-            model_name='eventip',
-            name='id',
-            field=models.BigAutoField(primary_key=True, serialize=False),
-        ),
+        migrations.DeleteModel('EventIP'),
+        migrations.RenameModel('TempEventIP', 'EventIP'),
+        migrations.AlterModelTable(name='eventip', table=None),
     ]
