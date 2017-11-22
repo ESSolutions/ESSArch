3ac587fd26bd444042096dd5c28fdeb8f8176ffa
Oskar Persson <oskar.persson@polken.se>
2017-11-22 12:52:03 +0100

    Add NestedViewSetMixin to ValidationViewSet

--- ESSArch_Core/fixity/views.py
+++ ESSArch_Core/fixity/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 
+from rest_framework_extensions.mixins import NestedViewSetMixin
+
 from ESSArch_Core.fixity.models import Validation
 from ESSArch_Core.fixity.serializers import ValidationSerializer
 
 
-class ValidationViewSet(viewsets.ReadOnlyModelViewSet):
+class ValidationViewSet(NestedViewSetMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Validation.objects.all()
     serializer_class = ValidationSerializer
