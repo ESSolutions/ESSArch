d89364537865035b0ead9f36a621c17fc304f826
Oskar Persson <oskar.persson@polken.se>
2017-12-08 11:45:50 +0100

    Refactor StructureValidator

--- ESSArch_Core/fixity/validation/backends/structure.py
+++ ESSArch_Core/fixity/validation/backends/structure.py
@@ -1,11 +1,9 @@
 import logging
 import os
 
-from glob2 import glob, iglob
-
-from scandir import scandir
-
 import six
+from glob2 import glob, iglob
+from scandir import walk
 
 from ESSArch_Core.exceptions import ValidationError
 from ESSArch_Core.fixity.validation.backends.base import BaseValidator
@@ -20,8 +18,31 @@ class StructureValidator(BaseValidator):
 
     file_validator = False
 
+    def in_valid_paths(self, root, path, valid_paths):
+        for valid_path in [p for p in valid_paths if isinstance(p, six.string_types)]:
+            if path in glob(valid_path):
+                return True
+
+        for valid_path in [p for p in valid_paths if not isinstance(p, six.string_types)]:
+            for nested_valid_path in valid_path:
+                for found_nested_path, matches in iglob(nested_valid_path, with_matches=True):
+                    if found_nested_path == path:
+                        # check matches
+                        for match in matches:
+                            for related_path in valid_path:
+                                if related_path != found_nested_path:
+                                    related_path = related_path.replace('*', match, 1)
+
+                                    if not os.path.isfile(related_path):
+                                        rel_path = os.path.relpath(path, root)
+                                        rel_related_path = os.path.relpath(related_path, root)
+                                        raise ValidationError('{file} missing related file {related}'.format(file=rel_path, related=rel_related_path))
+
+                        return True
+
+        raise ValidationError('{file} is not allowed'.format(file=path))
+
     def validate_folder(self, path, node):
-        valid_extensions = [ext.format(**self.data) for ext in node.get('valid_extensions', [])]
         valid_paths = node.get('valid_paths', [])
         required_files = [req.format(**self.data) for req in node.get('required_files', [])]
 
@@ -32,64 +53,22 @@ class StructureValidator(BaseValidator):
                 for nested_idx, nested_valid in enumerate(valid):
                     valid[nested_idx] = os.path.join(path, nested_valid).format(**self.data)
 
-        for entry in scandir(path):
-            if entry.is_dir():
-                continue
+        for root, dirs, files in walk(path):
+            for f in files:
+                if len(valid_paths):
+                    self.in_valid_paths(path, os.path.join(root, f), valid_paths)
 
-            try:
-                required_files.remove(entry.name)
-            except ValueError:
-                pass
-            else:
-                continue
-
-            if len(valid_paths):
-                valid = False
-                for valid_path in valid_paths:
-                    if isinstance(valid_path, six.string_types):
-                        if entry.path in glob(valid_path):
-                            valid = True
-                            break
+                if len(required_files):
+                    rel_dir = os.path.relpath(root, path)
+                    if rel_dir == '.':
+                        rel_file = f
                     else:
-                        # valid is a list of strings. The entry must match one
-                        # of them and the remaining strings (wildcards replaced)
-                        # must match a file too
+                        rel_file = os.path.join(rel_dir, f)
 
-                        found = False
-                        for nested_valid in valid_path:
-                            for nested_valid_path, matches in iglob(nested_valid, with_matches=True):
-                                if nested_valid_path == entry.path:
-                                    found = True
-                                    break
-
-                            if found:
-                                break
-
-                        if not found:
-                            break
-
-                        for nested_valid in valid_path:
-                            for match in matches:
-                                nested_valid = nested_valid.replace('*', match, 1)
-
-                            if not os.path.isfile(nested_valid):
-                                raise ValidationError('{file} missing related file {related}'.format(file=entry.path, related=nested_valid))
-
-                        valid = True
-
-                if not valid:
-                    raise ValidationError('{file} is not allowed'.format(file=entry.path))
-
-            filepath, ext = os.path.splitext(entry.path)
-            if len(valid_extensions) and not ext[1:] in valid_extensions:
-                raise ValidationError('{file} does not have a valid extension'.format(file=entry.path))
-
-            try:
-                # delete entry from required list
-                required_files.remove(entry.path)
-            except ValueError:
-                # the entry is not in the required list
-                pass
+                    try:
+                        required_files.remove(rel_file)
+                    except ValueError:
+                        pass
 
         if len(required_files):
             raise ValidationError('Missing {files} in {path}'.format(files=','.join(required_files), path=path))
