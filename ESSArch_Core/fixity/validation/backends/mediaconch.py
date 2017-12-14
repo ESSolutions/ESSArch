a777d8fd81f8485ee6a4b7d7e6376666d613bbb2
Oskar Persson <oskar.persson@polken.se>
2017-12-14 13:20:41 +0100

    Add output_format and reporting_element parameter to mediaconch

--- ESSArch_Core/fixity/validation/backends/mediaconch.py
+++ ESSArch_Core/fixity/validation/backends/mediaconch.py
@@ -9,8 +9,10 @@ from ESSArch_Core.fixity.validation.backends.base import BaseValidator
 logger = logging.getLogger('essarch.fixity.validation.mediaconch')
 
 
-def run_mediaconch(filename, policy=None):
-    cmd = 'mediaconch -fx -p {policy} {filename}'.format(policy=policy, filename=filename)
+def run_mediaconch(filename, reporting_element='Mediaconch', output_format='xml', policy=None):
+    cmd = 'mediaconch --{reporter} --Format={format} -p {policy} {filename}'.format(reporter=reporting_element,
+                                                                                    format=output_format, policy=policy,
+                                                                                    filename=filename)
     p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
     out, err = p.communicate()
     return out, err, p.returncode
@@ -28,7 +30,7 @@ def get_outcome(root):
 class MediaconchValidator(BaseValidator):
     def validate(self, filepath):
         policy = self.context
-        out, _, _ = run_mediaconch(filepath, policy)
+        out, _, _ = run_mediaconch(filepath, policy=policy)
 
         parser = etree.XMLParser(remove_blank_text=True)
         root = etree.XML(out, parser=parser)
