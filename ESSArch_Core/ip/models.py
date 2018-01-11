548ddfd6fe86673225d5a1232e0cd2e3c5cc3878
Oskar Persson <oskar.persson@polken.se>
2018-01-11 10:38:01 +0100

    Add code field to ArchivalInstitution

--- ESSArch_Core/ip/models.py
+++ ESSArch_Core/ip/models.py
@@ -86,6 +86,7 @@ MESSAGE_DIGEST_ALGORITHM_CHOICES = (
 class ArchivalInstitution(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
+    code = models.CharField(max_length=16, unique=True)  # ISO 15511
 
     class Meta:
         verbose_name = 'ArchivalInstitution'
