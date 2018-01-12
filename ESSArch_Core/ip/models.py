90d4ca8b838206041d1f08b9952124b72b4b80e3
Oskar Persson <oskar.persson@polken.se>
2018-01-12 11:59:12 +0100

    Add code field to ArchivistOrganization

--- ESSArch_Core/ip/models.py
+++ ESSArch_Core/ip/models.py
@@ -100,6 +100,7 @@ class ArchivalInstitution(models.Model):
 class ArchivistOrganization(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
+    code = models.CharField(max_length=255, unique=True, blank=True, null=True)
 
     class Meta:
         verbose_name = 'ArchivistOrganization'
