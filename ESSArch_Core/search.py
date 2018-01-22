c8e58e61d0c02ae7a43475c521f6b10db4884510
Oskar Persson <oskar.persson@polken.se>
2018-01-22 11:14:12 +0100

    Return ES connection

--- ESSArch_Core/search.py
+++ ESSArch_Core/search.py
@@ -4,11 +4,11 @@ DEFAULT_MAX_RESULT_WINDOW = 10000
 
 
 def connect(alias='default'):
-    connections.create_connection(alias)
+    return connections.create_connection(alias)
 
 
 def get_connection(alias='default'):
     try:
-        connections.get_connection(alias)
+        return connections.get_connection(alias)
     except:
-        connect(alias)
+        return connect(alias)
