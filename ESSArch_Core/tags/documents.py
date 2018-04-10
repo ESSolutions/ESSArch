16e989232b8fdec34b3f2425e95070cffc148584
Oskar Persson <oskar.persson@polken.se>
2018-04-11 00:22:36 +0200

    Have Document inherit from Component

--- ESSArch_Core/tags/documents.py
+++ ESSArch_Core/tags/documents.py
@@ -134,15 +134,8 @@ class InformationPackage(DocType):
         index = 'information_package'
 
 
-class Document(VersionedDocType):
-    id = Keyword()  # @id
+class Document(Component):
     ip = Keyword()
-    parent = Object(Node)  # component
-    reference_code = Keyword()
-    archive = Keyword()
-    institution = Keyword()
-    organization = Keyword()
-    name = Text(analyzer=ngram_analyzer, search_analyzer='standard')  # @title in 2002, @linktitle in 3.0
     extension = Keyword()
     href = Keyword()  # @href
     size = Long()
