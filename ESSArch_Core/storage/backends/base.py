4e2f0763d08db902ebee309a2f90eded400d9fc2
Oskar Persson <oskar.persson@polken.se>
2018-04-29 15:55:07 +0200

    Fix BaseStorageBackend error messages

--- ESSArch_Core/storage/backends/base.py
+++ ESSArch_Core/storage/backends/base.py
@@ -3,13 +3,13 @@ from contextlib import contextmanager
 class BaseStorageBackend(object):
     @contextmanager
     def open(self, storage_object, file, *args, **kwargs):
-        raise NotImplementedError('subclasses of BaseValidator must provide an open() method')
+        raise NotImplementedError('subclasses of BaseStorageBackend must provide an open() method')
 
     def read(self, storage_object, dst, extract=False, include_xml=True, block_size=None):
-        raise NotImplementedError('subclasses of BaseValidator must provide an read() method')
+        raise NotImplementedError('subclasses of BaseStorageBackend must provide an read() method')
 
     def write(self, src, ip, storage_method, storage_medium, block_size=None):
-        raise NotImplementedError('subclasses of BaseValidator must provide an write() method')
+        raise NotImplementedError('subclasses of BaseStorageBackend must provide an write() method')
 
     def delete(self, storage_object):
-        raise NotImplementedError('subclasses of BaseValidator must provide a delete() method')
+        raise NotImplementedError('subclasses of BaseStorageBackend must provide a delete() method')
