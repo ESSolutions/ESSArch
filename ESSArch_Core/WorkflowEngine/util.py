4df841789e33ead06b16785ea8627e50e08e1885
Oskar Persson <oskar.persson@polken.se>
2018-05-02 12:35:59 +0200

    Add create_workflow

--- ESSArch_Core/WorkflowEngine/util.py
+++ ESSArch_Core/WorkflowEngine/util.py
@@ -1,7 +1,7 @@
 from celery import states as celery_states
 from celery.result import AsyncResult
 
-from ESSArch_Core.WorkflowEngine.models import ProcessTask
+from ESSArch_Core.WorkflowEngine.models import ProcessStep, ProcessTask
 
 
 def get_result(pk, eager=False):
@@ -9,3 +9,39 @@ def get_result(pk, eager=False):
         return AsyncResult(str(pk)).result
     else:
         return ProcessTask.objects.values_list('result', flat=True).get(pk=pk, status=celery_states.SUCCESS)
+
+
+def _create_step(parent_step, flow, ip, responsible):
+    for t_idx, task in enumerate(flow):
+        if not task.get('if', True):
+            continue
+
+        if task.get('step', False):
+            child_s = ProcessStep.objects.create(
+                name=task['name'],
+                parent_step=parent_step,
+                parent_step_pos=t_idx,
+                information_package=ip,
+            )
+            _create_step(child_s, task['children'], ip, responsible)
+        else:
+            args = task.get('args', [])
+            params = task.get('params', {})
+            ProcessTask.objects.create(
+                name=task['name'],
+                label=task.get('label'),
+                args=args,
+                params=params,
+                eager=False,
+                information_package=ip,
+                responsible=responsible,
+                processstep=parent_step,
+                processstep_pos=t_idx,
+            )
+
+
+def create_workflow(workflow_spec, ip=None):
+    root_step = ProcessStep.objects.create(eager=False)
+    responsible = getattr(ip, 'responsible', None)
+    _create_step(root_step, workflow_spec, ip, responsible)
+    return root_step
