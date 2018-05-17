bffddb836a5675e9faa0e5e226aaa082e1b9b8cd
petrus <petrus.jonsson@gmail.com>
2018-05-17 11:23:36 +0200

    Add Notifications frontend

---
+++ ESSArch_Core/frontend/scripts/components/NotificationsComponent.js
@@ -0,0 +1,7 @@
+angular.module('myApp').component('notifications', {
+    templateUrl: 'notifications.html',
+    controller: 'NotificationsCtrl',
+    controllerAs: 'vm',
+    bindings: {
+    }
+  });
