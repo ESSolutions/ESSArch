4b289cb0f4a6167f4048f2891941c0e58d74868b
petrus <petrus.jonsson@gmail.com>
2018-05-31 00:08:44 +0200

    Add change organization modal and context menu base service

---
+++ ESSArch_Core/frontend/scripts/controllers/OrganizationModalInstanceCtrl.js
@@ -0,0 +1,35 @@
+angular.module('myApp').controller('OrganizationModalInstanceCtrl', function ($translate, $uibModalInstance, djangoAuth, appConfig, $http, data, $scope, Notifications, $timeout, Organization) {
+    var $ctrl = this;
+    $ctrl.ip = data.ip;
+    $ctrl.saving = false;
+    $ctrl.$onInit = function () {
+        $ctrl.currentOrganization = angular.copy(Organization.getOrganization().id);
+        $ctrl.organization  = Organization.getOrganization().id;
+        $ctrl.organizations = Organization.availableOrganizations();
+    }
+
+    $ctrl.save = function () {
+        $ctrl.saving = true;
+        $http({
+            method: 'POST',
+            url: appConfig.djangoUrl + 'information-packages/' + data.ip.id + '/change-organization/',
+            data: {
+                organization: $ctrl.organization
+            }
+        }).then(function (response) {
+            Notifications.add($translate.instant('ORGANIZATION_CHANGED'), 'success');
+            $uibModalInstance.close("changed");
+            $ctrl.saving = false;
+        }).catch(function (response) {
+            $ctrl.saving = false;
+            if (response.data.detail) {
+                Notifications.add(response.data.detail, 'error');
+            } else {
+                Notifications.add('Unknown error', 'error');
+            }
+        })
+    }
+    $ctrl.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    }
+})
---
+++ ESSArch_Core/frontend/scripts/services/ContextMenuBase.js
@@ -0,0 +1,13 @@
+angular.module('myApp').factory('ContextMenuBase', function ($translate) {
+    var service  = {
+        changeOrganization: function(callback){
+            return {
+                text: $translate.instant("CHANGE_ORGANIZATION"),
+                click: function ($itemScope, $event, modelValue, text, $li) {
+                    callback();
+                }
+            }
+        }
+    }
+    return service;
+});
