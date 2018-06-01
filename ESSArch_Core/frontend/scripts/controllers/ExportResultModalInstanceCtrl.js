575f0783e6cf23d3a6801196ca0ae5feb644ed7e
petrus <petrus.jonsson@gmail.com>
2018-06-01 11:50:17 +0200

    Add modal for exporting search result

---
+++ ESSArch_Core/frontend/scripts/controllers/ExportResultModalInstanceCtrl.js
@@ -0,0 +1,15 @@
+angular.module('myApp').controller('ExportResultModalInstanceCtrl', function ($translate, $uibModalInstance, djangoAuth, appConfig, $http, data, $scope, Notifications, $timeout, Search, $sce, $window) {
+    var $ctrl = this;
+    var vm = data.vm;
+    $ctrl.formats = ['pdf', 'csv'];
+    $ctrl.format = 'pdf';
+    $ctrl.exporting = false;
+    $ctrl.export = function () {
+        var file = $sce.trustAsResourceUrl(vm.getExportResultUrl(vm.tableState, $ctrl.format));
+        $window.open(file, '_blank');
+        $uibModalInstance.close();
+    }
+    $ctrl.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    }
+})
