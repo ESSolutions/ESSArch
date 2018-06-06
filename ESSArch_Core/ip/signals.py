7c23360262785475ae8f61205358fd6c719251fa
Oskar Persson <oskar.persson@polken.se>
2018-06-06 18:11:02 +0200

    Fix check if IP has AIC before trying to delete the AIC

--- ESSArch_Core/ip/signals.py
+++ ESSArch_Core/ip/signals.py
@@ -20,7 +20,7 @@ def ip_pre_delete(sender, instance, using, **kwargs):
 def ip_post_delete(sender, instance, using, **kwargs):
     logger.info('Information package %s was deleted' % instance.pk)
 
-    if instance.aic is not None and not instance.aic.information_packages.exists():
+    if getattr(instance, 'aic') is not None and not instance.aic.information_packages.exists():
         # this was the last IP in the AIC, delete the AIC as well
         instance.aic.delete()
 
