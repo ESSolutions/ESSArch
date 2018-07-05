c037bbf4c40cc98f4e21402d4d0f894355361aef
Oskar Persson <oskar.persson@polken.se>
2018-07-05 16:23:10 +0200

    Add GetObjectForUpdateViewMixin

--- ESSArch_Core/mixins.py
+++ ESSArch_Core/mixins.py
@@ -1,4 +1,6 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.settings import api_settings
+
 
 class PaginatedViewMixin(object):
     pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
@@ -29,3 +31,34 @@ class PaginatedViewMixin(object):
         """
         assert self.paginator is not None
         return self.paginator.get_paginated_response(data)
+
+
+class GetObjectForUpdateViewMixin(object):
+    def get_object_for_update(self):
+        """
+        Returns the object the view is displaying using a queryset with
+        `select_for_update`.
+
+        You may want to override this if you need to provide non-standard
+        queryset lookups.  Eg if objects are referenced using multiple
+        keyword arguments in the url conf.
+        """
+        queryset = self.filter_queryset(self.get_queryset()).select_for_update()
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+                'Expected view %s to be called with a URL keyword argument '
+                'named "%s". Fix your URL conf, or set the `.lookup_field` '
+                'attribute on the view correctly.' %
+                (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
