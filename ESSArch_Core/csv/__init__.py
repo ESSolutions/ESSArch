3675de165e8da45db2b9a69f575b62a910ef80b6
Oskar Persson <oskar.persson@polken.se>
2018-08-19 00:11:02 +0200

    Add UnicodeCSVReader

--- ESSArch_Core/csv/__init__.py
+++ ESSArch_Core/csv/__init__.py
@@ -6,6 +6,40 @@ except ImportError:
 import codecs
 import csv
 
+import six
+
+
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+
+class UnicodeCSVReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [six.text_type(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
 
 class UnicodeCSVWriter:
     """
