3bb92297e36ede7942617a9f6ecccbcf4b2d7674
Oskar Persson <oskar.persson@polken.se>
2018-09-25 23:42:07 +0200

    Add translateProvider and LanguageCtrl

---
+++ ESSArch_Core/frontend/scripts/controllers/LanguageCtrl.js
@@ -0,0 +1,18 @@
+angular.module('myApp').controller('LanguageCtrl', function($scope, $rootScope, $cookies, $cookieStore, $translate) {
+    $scope.changeLanguage = function(lang) {
+        $translate.use(lang);
+        moment.locale(lang);
+    }
+    $scope.getCurrentLanguage = function() {
+        var lang = $cookies.get('essarch_language');
+        $scope.currentLanguage = lang;
+        moment.locale(lang);
+        return lang;
+    }
+    $scope.getCurrentLanguage();
+
+    $scope.loadLanguages = function() {
+        $scope.availableLanguages = $translate.getAvailableLanguageKeys();
+    }
+    $scope.loadLanguages();
+});
