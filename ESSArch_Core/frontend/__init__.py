b334ea808ebfe798539818503677534ce32770b9
Oskar Persson <oskar.persson@polken.se>
2018-09-27 17:02:34 +0200

    Add home route and view

--- ESSArch_Core/frontend/__init__.py
+++ ESSArch_Core/frontend/__init__.py
@@ -1,1 +1,12 @@
+from django.conf import settings
+from django.shortcuts import redirect, render, reverse
+
 default_app_config = 'ESSArch_Core.frontend.apps.FrontendConfig'
+
+
+def home(req):
+    if req.GET.get('ref') != 'logout' and not req.user.is_authenticated:
+        if getattr(settings, 'ENABLE_ADFS_LOGIN', False):
+            return redirect(reverse('saml2:saml2_login'))
+
+    return render(req, 'essarch_core_frontend/main.html')
