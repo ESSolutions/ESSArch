d744d1bbdf46699b471189cc985e6a8852bb71bd
Oskar Persson <oskar.persson@polken.se>
2018-10-15 16:58:04 +0200

    Add InformationPackageViewSet

--- ESSArch_Core/ip/views.py
+++ ESSArch_Core/ip/views.py
@@ -1,17 +1,27 @@
-import six
+import itertools
+import os
+
+from django.db.models import Prefetch
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import exceptions, filters, mixins, status, viewsets
+from rest_framework import exceptions, filters, mixins, permissions, status, viewsets
 from rest_framework.decorators import detail_route
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.response import Response
 from rest_framework_extensions.mixins import NestedViewSetMixin
+import six
 
+from ESSArch_Core.WorkflowEngine.models import ProcessStep, ProcessTask
+from ESSArch_Core.WorkflowEngine.serializers import ProcessStepChildrenSerializer
+from ESSArch_Core.auth.serializers import ChangeOrganizationSerializer
+from ESSArch_Core.filters import string_to_bool
 from ESSArch_Core.fixity.transformation import AVAILABLE_TRANSFORMERS
 from ESSArch_Core.fixity.validation import AVAILABLE_VALIDATORS
-from ESSArch_Core.ip.filters import AgentFilter, EventIPFilter
+from ESSArch_Core.ip.filters import AgentFilter, EventIPFilter, InformationPackageFilter
 from ESSArch_Core.ip.models import Agent, EventIP, InformationPackage, Workarea
+from ESSArch_Core.ip.permissions import CanChangeSA, CanDeleteIP
 from ESSArch_Core.ip.serializers import AgentSerializer, EventIPSerializer, WorkareaSerializer
-from ESSArch_Core.WorkflowEngine.models import ProcessStep, ProcessTask
+from ESSArch_Core.profiles.models import Profile, ProfileIP
 
 
 class AgentViewSet(viewsets.ModelViewSet):
@@ -163,3 +173,119 @@ class WorkareaEntryViewSet(mixins.DestroyModelMixin, viewsets.ReadOnlyModelViewS
             return Response(status=status.HTTP_204_NO_CONTENT)
 
         return super(WorkareaEntryViewSet, self).destroy(request, pk, **kwargs)
+
+
+class InformationPackageViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows information packages to be viewed or edited.
+    """
+    queryset = InformationPackage.objects.none()
+    filter_backends = (filters.OrderingFilter, DjangoFilterBackend, filters.SearchFilter,)
+    ordering_fields = (
+        'label', 'responsible', 'create_date', 'state',
+        'id', 'object_identifier_value', 'start_date', 'end_date',
+    )
+    search_fields = (
+        'object_identifier_value', 'label', 'responsible__first_name',
+        'responsible__last_name', 'responsible__username', 'state',
+        'submission_agreement__name', 'start_date', 'end_date',
+    )
+    filter_class = InformationPackageFilter
+
+    def get_permissions(self):
+        if self.action in ['partial_update', 'update']:
+            if self.request.data.get('submission_agreement'):
+                self.permission_classes = [CanChangeSA]
+        if self.action == 'destroy':
+            self.permission_classes = [CanDeleteIP]
+
+        return super(InformationPackageViewSet, self).get_permissions()
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+                'Expected view %s to be called with a URL keyword argument '
+                'named "%s". Fix your URL conf, or set the `.lookup_field` '
+                'attribute on the view correctly.' %
+                (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        lookup_field = self.lookup_field
+
+        objid = self.request.query_params.get('objid')
+        if objid is not None:
+            lookup_field = 'object_identifier_value'
+
+        filter_kwargs = {lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = InformationPackage.objects.visible_to_user(user)
+        queryset = queryset.prefetch_related(
+            Prefetch('profileip_set', to_attr='profiles'), 'profiles__profile', 'agents',
+            'responsible__user_permissions', 'responsible__groups__permissions', 'steps',
+        ).select_related('submission_agreement')
+        return queryset
+
+    @detail_route(methods=['post'], url_path='change-organization')
+    def change_organization(self, request, pk=None):
+        ip = self.get_object()
+
+        serializer = ChangeOrganizationSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        org = serializer.validated_data['organization']
+
+        ip.change_organization(org)
+        return Response()
+
+    @detail_route()
+    def workflow(self, request, pk=None):
+        ip = self.get_object()
+        hidden = request.query_params.get('hidden')
+
+        steps = ip.steps.filter(parent_step__information_package__isnull=True)
+        tasks = ip.processtask_set.filter(processstep__information_package__isnull=True)
+
+        if hidden is not None:
+            steps = steps.filter(hidden=string_to_bool(hidden))
+            tasks = tasks.filter(hidden=string_to_bool(hidden))
+
+        flow = sorted(itertools.chain(steps, tasks), key=lambda x: (x.get_pos(), x.time_created))
+
+        serializer = ProcessStepChildrenSerializer(data=flow, many=True, context={'request': request})
+        serializer.is_valid()
+        return Response(serializer.data)
+
+    @detail_route(methods=['put'], url_path='check-profile')
+    def check_profile(self, request, pk=None):
+        ip = self.get_object()
+        ptype = request.data.get("type")
+        pip = get_object_or_404(ProfileIP, ip=ip, profile__profile_type=ptype)
+
+        if not pip.LockedBy:
+            pip.included = request.data.get('checked', not pip.included)
+            pip.save()
+
+        return Response()
+
+    @detail_route(methods=['put'], url_path='change-profile')
+    def change_profile(self, request, pk=None):
+        ip = self.get_object()
+        new_profile = get_object_or_404(Profile, pk=request.data.get("new_profile"))
+
+        try:
+            ip.change_profile(new_profile)
+        except ValueError as e:
+            raise exceptions.ParseError(e.message)
+
+        return Response({'detail': 'Updating IP (%s) with new profile (%s)' % (ip.pk, new_profile)})
