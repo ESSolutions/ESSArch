423009a44b933ebfd406305c167505c7981d42ed
Oskar Persson <oskar.persson@polken.se>
2018-10-25 11:31:56 +0200

    Use custom implementation to avoid global variables

--- ESSArch_Core/frontend/templatetags/rev.py
+++ ESSArch_Core/frontend/templatetags/rev.py
@@ -1,6 +1,37 @@
+import json
+import os
+
 from django import template
+from django.conf import settings
 from django.templatetags.static import StaticNode
-from gulp_rev import production_url
+
+
+def _get_mapping():
+    """
+    Finds and loads gulp's rev-manifest.json file. Use DJANGO_GULP_REV_PATH to
+    set the path.
+    """
+
+    manifest_path = getattr(settings,
+        'DJANGO_GULP_REV_PATH',
+        os.path.join(getattr(settings, 'STATIC_ROOT', ''), 'rev-manifest.json'))
+
+    try:
+        with open(manifest_path) as manifest_file:
+            return json.load(manifest_file)
+    except IOError:
+        return None
+
+
+def _create_url(path, original):
+    mapping = _get_mapping()
+    if mapping:
+        if path in mapping:
+            return original.replace(path, mapping[path])
+        return original
+    else:
+        return ''
+
 
 def static_rev(path):
     """
@@ -8,7 +39,8 @@ def static_rev(path):
     """
 
     static_path = StaticNode.handle_simple(path)
-    return production_url(path, static_path)
+    return _create_url(path, static_path)
+
 
 register = template.Library()
 register.simple_tag(static_rev, name='rev')
