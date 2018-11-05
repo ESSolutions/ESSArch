4fbfc60b5622a376022aa31a421267ebcb62cf28
Pettanj <petrus.jonsson@gmail.com>
2018-11-05 15:28:27 +0100

    Add import component and overwrite modal templates

---
+++ ESSArch_Core/frontend/scripts/components/ImportComponent.js
@@ -0,0 +1,8 @@
+angular.module('essarch.components').component('import', {
+  templateUrl: 'import.html',
+  controller: 'ImportCtrl',
+  controllerAs: 'vm',
+  bindings: {
+      types: '<'
+  }
+});
