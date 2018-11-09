15d5727ae0ddf7d865019be5c04f348671f1ff51
Oskar Persson <oskar.persson@polken.se>
2018-11-09 11:15:12 +0100

    Use default mimetypes when custom file is missing (#74)

--- ESSArch_Core/fixity/test_format.py
+++ ESSArch_Core/fixity/test_format.py
@@ -19,11 +19,21 @@ class FormatIdentifierMimeTypeTests(TestCase):
         fid = FormatIdentifier(allow_unknown_file_types=True)
         self.assertEqual(fid.get_mimetype('foo.tar.gz'), 'application/gzip')
 
+    @mock.patch("ESSArch_Core.fixity.format.os.path.isfile", return_value=True)
     @mock.patch("ESSArch_Core.fixity.format.mimetypes.init")
-    def test_custom_list(self, mock_mimetypes_init):
+    def test_custom_list(self, mock_mimetypes_init, isfile):
         mimetypes_file = Path.objects.create(
             entity="path_mimetypes_definitionfile", value='path/to/mime.types')
         fid = FormatIdentifier(allow_unknown_file_types=True)
         fid._init_mimetypes()
         mock_mimetypes_init.assert_called_once_with(
             files=[mimetypes_file.value])
+
+    @mock.patch("ESSArch_Core.fixity.format.os.path.isfile", return_value=False)
+    @mock.patch("ESSArch_Core.fixity.format.mimetypes.init")
+    def test_custom_list_missing_file(self, mock_mimetypes_init, isfile):
+        mimetypes_file = Path.objects.create(
+            entity="path_mimetypes_definitionfile", value='path/to/mime.types')
+        fid = FormatIdentifier(allow_unknown_file_types=True)
+        fid._init_mimetypes()
+        mock_mimetypes_init.assert_called_once_with()
