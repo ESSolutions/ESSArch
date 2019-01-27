ad8fc41e8e770baa2b923d4d645a3d0947b57105
Oskar Persson <oskar.persson@polken.se>
2019-01-27 18:49:01 +0100

    Fix all F811 violations

--- ESSArch_Core/WorkflowEngine/views.py
+++ ESSArch_Core/WorkflowEngine/views.py
@@ -36,7 +36,6 @@ from rest_framework.decorators import detail_route
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.response import Response
-from rest_framework.permissions import DjangoModelPermissions
 
 from rest_framework_extensions.mixins import NestedViewSetMixin
 
