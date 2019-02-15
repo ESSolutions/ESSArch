c88d3a981aeb3dfd6d8bc2aee2f961f64bacd1e0
Oskar Persson <oskar.persson@polken.se>
2019-02-15 11:46:53 +0100

    Sort ProcessTask status choices

--- ESSArch_Core/WorkflowEngine/models.py
+++ ESSArch_Core/WorkflowEngine/models.py
@@ -544,9 +544,11 @@ class OrderedProcessTaskManager(models.Manager):
 
 
 class ProcessTask(Process):
-    TASK_STATE_CHOICES = zip(
+    _states = list(zip(
         celery_states.ALL_STATES, celery_states.ALL_STATES
-    )
+    ))
+    _states.sort()
+    TASK_STATE_CHOICES = _states
 
     label = models.CharField(max_length=255, blank=True)
     status = models.CharField(
