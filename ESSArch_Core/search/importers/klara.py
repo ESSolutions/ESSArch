ef3bc517758642dbc7743b4e819f5d35aaa79d57
Oskar Persson <oskar.persson@polken.se>
2019-02-25 13:35:31 +0100

    Refactor and do some initial testing of KlaraImporter

--- ESSArch_Core/search/importers/klara.py
+++ ESSArch_Core/search/importers/klara.py
@@ -35,13 +35,57 @@ class KlaraImporter(BaseImporter):
     AGENT_NAME_TYPE = 'auktoriserad'
     AGENT_ALT_NAME_TYPE = 'alternativt'
     COUNTRY_CODE = 'SE'
+    LANGUAGE_CODE = 'sv'
     REPO_CODE = 'C020'  # TODO: just a dummy
 
-    REF_CODE, _ = RefCode.objects.get_or_create(
-        country=Country.objects.get(iso=COUNTRY_CODE),
-        repository_code=REPO_CODE,
-    )
-    LANGUAGE = Language.objects.get(iso_639_1='sv')
+    _ref_code = None
+    _language = None
+    _name_type = None
+    _alt_name_type = None
+    _note_type_anmarkning = None
+    _note_type_historik = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def ref_code(self):
+        if self._ref_code is None:
+            self._ref_code, _ = RefCode.objects.get_or_create(
+                country=Country.objects.get(iso=self.COUNTRY_CODE),
+                repository_code=self.REPO_CODE,
+            )
+        return self._ref_code
+
+    @property
+    def language(self):
+        if self._language is None:
+            self._language = Language.objects.get(iso_639_1=self.LANGUAGE_CODE)
+        return self._language
+
+    @property
+    def name_type(self):
+        if self._name_type is None:
+            self._name_type, _ = AgentNameType.objects.get_or_create(name=self.AGENT_NAME_TYPE)
+        return self._name_type
+
+    @property
+    def alt_name_type(self):
+        if self._alt_name_type is None:
+            self._alt_name_type, _ = AgentNameType.objects.get_or_create(name=self.AGENT_ALT_NAME_TYPE)
+        return self._alt_name_type
+
+    @property
+    def note_type_anmarkning(self):
+        if self._note_type_anmarkning is None:
+            self._note_type_anmarkning, _ = AgentNoteType.objects.get_or_create(name='administrativ anmärkning')
+        return self._note_type_anmarkning
+
+    @property
+    def note_type_historik(self):
+        if self._note_type_historik is None:
+            self._note_type_historik, _ = AgentNoteType.objects.get_or_create(name='historik')
+        return self._note_type_historik
 
     @staticmethod
     def _parse_timestamp(ts):
@@ -74,39 +118,31 @@ class KlaraImporter(BaseImporter):
         return agent_type
 
     @classmethod
-    def parse_agent_alternative_names(cls, arkivbildare, agent):
-        agent_name_type, _ = AgentNameType.objects.get_or_create(name=cls.AGENT_ALT_NAME_TYPE)
-
+    def _parse_agent_alternative_names(cls, arkivbildare, agent, name_type):
         for name_el in arkivbildare.xpath("ObjectParts/AltNames/ArchiveOrig.AltNames/ArchOrigAltName"):
             alt_name = AgentName.objects.create(
                 agent=agent,
                 main=name_el.xpath("ArchOrigAltName.Name")[0].text,
-                type=agent_name_type,
+                type=name_type,
                 start_date=datetime.strptime(name_el.xpath("ArchOrigAltName.UsedFrom")[0].text, '%Y'),
                 end_date=datetime.strptime(name_el.xpath("ArchOrigAltName.UsedTo")[0].text, '%Y'),
             )
             yield alt_name
 
     @classmethod
-    def parse_agent_names(cls, arkivbildare, agent):
-        agent_name_type, _ = AgentNameType.objects.get_or_create(name=cls.AGENT_NAME_TYPE)
-
+    def parse_agent_names(cls, arkivbildare, agent, name_type, alt_name_type):
         authorized_name = AgentName.objects.create(
             agent=agent,
             main=arkivbildare.xpath("ObjectParts/General/ArchiveOrig.Name")[0].text,
-            type=agent_name_type,
+            type=name_type,
         )
 
-        alt_names = list(cls.parse_agent_alternative_names(arkivbildare, agent))
+        alt_names = list(cls._parse_agent_alternative_names(arkivbildare, agent, alt_name_type))
         return [authorized_name] + alt_names
 
     @classmethod
-    def parse_agent_notes(cls, arkivbildare, agent):
+    def parse_agent_notes(cls, arkivbildare, agent, anmarkning_type, historik_type):
         notes = []
-
-        note_type_admin_anmerkning, _ = AgentNoteType.objects.get_or_create(
-            name='administrativ anmärkning'
-        )
 
         create_date = cls.parse_agent_create_date(arkivbildare)
         created_by = cls.parse_agent_created_by(arkivbildare)
@@ -119,7 +155,7 @@ class KlaraImporter(BaseImporter):
         )
         created_note = AgentNote.objects.create(
             agent=agent,
-            type=note_type_admin_anmerkning,
+            type=anmarkning_type,
             text=created_text,
             create_date=timezone.now(),
         )
@@ -136,7 +172,7 @@ class KlaraImporter(BaseImporter):
         )
         revised_note = AgentNote.objects.create(
             agent=agent,
-            type=note_type_admin_anmerkning,
+            type=anmarkning_type,
             text=revised_text,
             create_date=timezone.now(),
         )
@@ -146,12 +182,9 @@ class KlaraImporter(BaseImporter):
         if len(historik) and historik[0].text:
             historik = historik[0].text
 
-            note_type_historik, _ = AgentNoteType.objects.get_or_create(
-                name='historik',
-            )
             note = AgentNote.objects.create(
                 agent=agent,
-                type=note_type_historik,
+                type=historik_type,
                 text=historik,
                 create_date=cls.parse_agent_revise_date(arkivbildare),
                 revise_date=cls.parse_agent_revise_date(arkivbildare),
@@ -255,28 +288,27 @@ class KlaraImporter(BaseImporter):
 
         return end_date
 
-    @classmethod
-    def parse_arkivbildare(cls, el, task=None):
+    def parse_arkivbildare(self, el, task=None):
         logger.debug("Parsing arkivbildare...")
 
-        agent_type = cls.parse_agent_type(el)
+        agent_type = self.parse_agent_type(el)
 
-        level_of_detail = cls.parse_agent_level_of_detail(el)
-        record_status = cls.parse_agent_record_status(el)
+        level_of_detail = self.parse_agent_level_of_detail(el)
+        record_status = self.parse_agent_record_status(el)
 
-        create_date = cls.parse_agent_create_date(el)
-        revise_date = cls.parse_agent_revise_date(el)
+        create_date = self.parse_agent_create_date(el)
+        revise_date = self.parse_agent_revise_date(el)
 
-        start_date = cls.parse_agent_start_date(el)
-        end_date = cls.parse_agent_end_date(el)
+        start_date = self.parse_agent_start_date(el)
+        end_date = self.parse_agent_end_date(el)
 
         agent = Agent.objects.create(
             type=agent_type,
-            ref_code=cls.REF_CODE,
+            ref_code=self.ref_code,
             level_of_detail=level_of_detail,
             record_status=record_status,
             script=Agent.LATIN,
-            language=cls.LANGUAGE,
+            language=self.language,
             create_date=create_date,
             revise_date=revise_date,
             start_date=start_date,
@@ -284,10 +316,10 @@ class KlaraImporter(BaseImporter):
             task=task,
         )
 
-        cls.parse_agent_names(el, agent)
-        cls.parse_agent_notes(el, agent)
-        cls.parse_agent_identifiers(el, agent)
-        cls.parse_agent_places(el, agent)
+        self.parse_agent_names(el, agent, self.name_type, self.alt_name_type)
+        self.parse_agent_notes(el, agent, self.note_type_anmarkning, self.note_type_historik)
+        self.parse_agent_identifiers(el, agent)
+        self.parse_agent_places(el, agent)
 
         logger.info("Parsed arkivbildare: {}".format(agent.pk))
 
