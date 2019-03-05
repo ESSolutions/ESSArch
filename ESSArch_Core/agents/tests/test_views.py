e29c093306efc0a65bccc1e903beb520b8092cea
Oskar Persson <oskar.persson@polken.se>
2019-03-05 16:36:12 +0100

    More agent view tests

--- ESSArch_Core/agents/tests/test_views.py
+++ ESSArch_Core/agents/tests/test_views.py
@@ -29,6 +29,58 @@ from ESSArch_Core.agents.models import (
 User = get_user_model()
 
 
+class ListAgentTests(TestCase):
+    fixtures = ['countries_data', 'languages_data']
+
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('agent-list')
+
+        self.user = User.objects.create(username='user')
+        self.member = self.user.essauth_member
+
+        self.client.force_authenticate(user=self.user)
+
+        self.main_agent_type = MainAgentType.objects.create()
+        self.agent_type = AgentType.objects.create(main_type=self.main_agent_type)
+
+        self.ref_code = RefCode.objects.create(
+            country=Country.objects.get(iso='SE'),
+            repository_code='repo',
+        )
+
+    def create_agent(self):
+        return Agent.objects.create(
+            level_of_detail=Agent.MINIMAL,
+            script=Agent.LATIN,
+            language=Language.objects.get(iso_639_1='sv'),
+            record_status=Agent.DRAFT,
+            type=self.agent_type,
+            ref_code=self.ref_code,
+            create_date=timezone.now(),
+        )
+
+    def test_empty(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, [])
+
+    def test_one_agent(self):
+        self.create_agent()
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_multiple_agents(self):
+        self.create_agent()
+        self.create_agent()
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+
 class CreateAgentTests(TestCase):
     fixtures = ['countries_data', 'languages_data']
 
@@ -67,7 +119,7 @@ class CreateAgentTests(TestCase):
             create_date=timezone.now(),
         )
 
-    def test_create(self):
+    def test_create_complete(self):
         related_agent = self.create_agent()
 
         response = self.client.post(
@@ -176,6 +228,45 @@ class CreateAgentTests(TestCase):
         )
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateAgentTests(TestCase):
+    fixtures = ['countries_data', 'languages_data']
+
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('agent-list')
+
+        self.user = User.objects.create(username='user')
+        self.member = self.user.essauth_member
+
+        self.client.force_authenticate(user=self.user)
+
+        self.main_agent_type = MainAgentType.objects.create()
+        self.agent_type = AgentType.objects.create(main_type=self.main_agent_type)
+
+        self.authority_type = AuthorityType.objects.create(name='test')
+        self.identifier_type = AgentIdentifierType.objects.create(name='test')
+        self.name_type = AgentNameType.objects.create(name='test')
+        self.note_type = AgentNoteType.objects.create(name='test')
+        self.place_type = AgentPlaceType.objects.create(name='test')
+        self.relation_type = AgentRelationType.objects.create(name='test')
+
+        self.ref_code = RefCode.objects.create(
+            country=Country.objects.get(iso='SE'),
+            repository_code='repo',
+        )
+
+    def create_agent(self):
+        return Agent.objects.create(
+            level_of_detail=Agent.MINIMAL,
+            script=Agent.LATIN,
+            language=Language.objects.get(iso_639_1='sv'),
+            record_status=Agent.DRAFT,
+            type=self.agent_type,
+            ref_code=self.ref_code,
+            create_date=timezone.now(),
+        )
 
     def test_update_names(self):
         agent = self.create_agent()
