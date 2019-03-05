8c5cfe79a54ef47ff72c96fdfd8284287481aaae
haniffm <haniffm@kth.se>
2019-03-05 11:35:06 +0100

    Fix find_all_files on Windows

--- ESSArch_Core/maintenance/tests/test_views.py
+++ ESSArch_Core/maintenance/tests/test_views.py
@@ -10,6 +10,7 @@ from rest_framework.test import APIClient
 
 from ESSArch_Core.ip.models import InformationPackage
 from ESSArch_Core.maintenance.views import find_all_files
+from ESSArch_Core.util import win_to_posix
 
 User = get_user_model()
 
@@ -76,12 +77,15 @@ class FindAllFilesTests(TestCase):
 
         return files
 
+    def normalize_paths(self, expected_file_names):
+        return [win_to_posix(f) for f in expected_file_names]
+
     def test_find_all_files_when_pattern_is_star(self):
         files = self.create_dirs_and_files()
-        expected_file_names = [fi.replace(os.path.join(self.datadir, ""), "") for fi in files]
+        expected_file_names = self.normalize_paths([fi.replace(os.path.join(self.datadir, ""), "") for fi in files])
 
         found_files = find_all_files(self.datadir, self.ip, "*")
-        docs = [e['document'] for e in found_files]
+        docs = self.normalize_paths([e['document'] for e in found_files])
 
         for e in found_files:
             self.assertEqual(e['ip'], 'ip_obj_id_value')
@@ -90,15 +94,15 @@ class FindAllFilesTests(TestCase):
 
     def test_find_all_files_when_pattern_is_matching_pattern_which_ends_with_star(self):
         self.create_dirs_and_files()
-        expected_file_names = [
+        expected_file_names = self.normalize_paths([
             'a_dir/0.txt', 'a_dir/1.txt', 'a_dir/2.txt',
             'aa_dir/0.txt', 'aa_dir/1.txt', 'aa_dir/2.txt',
             'ab_dir/0.txt', 'ab_dir/1.txt', 'ab_dir/2.txt',
             'ac_dir/0.txt', 'ac_dir/1.txt', 'ac_dir/2.txt',
             'a_dir/ca_dir/0.txt', 'a_dir/ca_dir/1.txt', 'a_dir/ca_dir/2.txt',
-        ]
+        ])
         found_files = find_all_files(self.datadir, self.ip, "a*")
-        docs = [e['document'] for e in found_files]
+        docs = self.normalize_paths([e['document'] for e in found_files])
 
         for e in found_files:
             self.assertEqual(e['ip'], 'ip_obj_id_value')
@@ -106,13 +110,13 @@ class FindAllFilesTests(TestCase):
 
     def test_find_all_files_when_pattern_is_matching_pattern_which_start_with_star(self):
         self.create_dirs_and_files()
-        expected_file_names = [
+        expected_file_names = self.normalize_paths([
             'a_dir/2.txt', 'aa_dir/2.txt', 'ab_dir/2.txt', 'ac_dir/2.txt', 'a_dir/ca_dir/2.txt',
             'b_dir/2.txt', 'ba_dir/2.txt', 'bb_dir/2.txt', 'bc_dir/2.txt', 'b_dir/cb_dir/2.txt',
             'c_dir/2.txt', 'ca_dir/2.txt', 'cb_dir/2.txt', 'cc_dir/2.txt',
-        ]
+        ])
         found_files = find_all_files(self.datadir, self.ip, "**/2.txt")
-        docs = [e['document'] for e in found_files]
+        docs = self.normalize_paths([e['document'] for e in found_files])
 
         for e in found_files:
             self.assertEqual(e['ip'], 'ip_obj_id_value')
@@ -120,13 +124,13 @@ class FindAllFilesTests(TestCase):
 
     def test_find_all_files_when_pattern_is_matching_pattern_which_starts_and_ends_with_star(self):
         self.create_dirs_and_files()
-        expected_file_names = [
+        expected_file_names = self.normalize_paths([
             'a_dir/2.txt', 'aa_dir/2.txt', 'ab_dir/2.txt', 'ac_dir/2.txt', 'a_dir/ca_dir/2.txt',
             'b_dir/2.txt', 'ba_dir/2.txt', 'bb_dir/2.txt', 'bc_dir/2.txt', 'b_dir/cb_dir/2.txt',
             'c_dir/2.txt', 'ca_dir/2.txt', 'cb_dir/2.txt', 'cc_dir/2.txt',
-        ]
+        ])
         found_files = find_all_files(self.datadir, self.ip, "**/2*")
-        docs = [e['document'] for e in found_files]
+        docs = self.normalize_paths([e['document'] for e in found_files])
 
         for e in found_files:
             self.assertEqual(e['ip'], 'ip_obj_id_value')
