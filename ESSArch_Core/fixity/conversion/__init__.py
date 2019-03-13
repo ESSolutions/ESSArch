826f81c704932775b5aea058513a4422997836b2
Oskar Persson <oskar.persson@polken.se>
2019-03-13 13:35:33 +0100

    Add OpenSSLConverter (#253)

---
+++ ESSArch_Core/fixity/conversion/__init__.py
@@ -0,0 +1,19 @@
+import importlib
+
+from django.conf import settings
+
+AVAILABLE_CONVERTERS = {
+    'openssl': 'ESSArch_Core.fixity.conversion.backends.openssl.OpenSSLConverter',
+}
+
+extra_converters = getattr(settings, 'ESSARCH_CONVERTERS', {})
+AVAILABLE_CONVERTERS.update(extra_converters)
+
+
+def get_backend(name, *args, **kwargs):
+    try:
+        module_name, klass = AVAILABLE_CONVERTERS[name].rsplit('.', 1)
+    except KeyError:
+        raise ValueError('Converter backend "%s" not found' % name)
+
+    return getattr(importlib.import_module(module_name), klass)(*args, **kwargs)
