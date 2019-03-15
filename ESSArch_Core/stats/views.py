5a00d30d180252f467025592657309718398674b
Oskar Persson <oskar.persson@polken.se>
2019-03-15 09:42:23 +0100

    Add StatsView

---
+++ ESSArch_Core/stats/views.py
@@ -0,0 +1,29 @@
+from celery import states as celery_states
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+from django.db.models import Sum
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ESSArch_Core.auth.models import GroupMemberRole
+from ESSArch_Core.ip.models import InformationPackage
+from ESSArch_Core.maintenance.models import AppraisalJob
+from ESSArch_Core.tags.models import TagVersion
+
+User = get_user_model()
+
+
+class StatsView(APIView):
+    def get(self, request):
+        data = {
+            'appraisals': AppraisalJob.objects.filter(status=celery_states.SUCCESS).count(),
+            'information_packages': InformationPackage.objects.count(),
+            'ordered_information_packages': InformationPackage.objects.filter(orders__isnull=False).count(),
+            'permissions': Permission.objects.count(),
+            'roles': GroupMemberRole.objects.count(),
+            'tags': TagVersion.objects.exclude(elastic_index='archive').count(),
+            'total_object_size': InformationPackage.objects.aggregate(Sum('object_size'))['object_size__sum'] or 0,
+            'users': User.objects.count(),
+        }
+
+        return Response(data)
