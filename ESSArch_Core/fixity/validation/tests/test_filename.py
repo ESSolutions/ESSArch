99c37b876fce79e298e844a072cb0f410feb8733
Oskar Persson <oskar.persson@polken.se>
2019-03-18 12:01:42 +0100

    Fix default validation of directory names

--- ESSArch_Core/fixity/validation/tests/test_filename.py
+++ ESSArch_Core/fixity/validation/tests/test_filename.py
@@ -1,3 +1,6 @@
+import shutil
+import tempfile
+
 from django.test import TestCase
 
 from ESSArch_Core.exceptions import ValidationError
@@ -6,17 +9,41 @@ from ESSArch_Core.fixity.validation.backends.filename import FilenameValidator
 
 
 class FilenameValidatorTests(TestCase):
+    def setUp(self):
+        self.datadir = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, self.datadir)
+
     def test_valid(self):
-        validator = FilenameValidator()
-        validator.validate('foo.xml')
+        with tempfile.NamedTemporaryFile(dir=self.datadir, suffix='.pdf') as f:
+            validator = FilenameValidator()
+            validator.validate(f.name)
+
+        self.assertEqual(Validation.objects.count(), 1)
+        self.assertEqual(Validation.objects.filter(passed=True).count(), 1)
+
+    def test_valid_directory(self):
+        with tempfile.TemporaryDirectory(dir=self.datadir) as f:
+            validator = FilenameValidator()
+            validator.validate(f)
 
         self.assertEqual(Validation.objects.count(), 1)
         self.assertEqual(Validation.objects.filter(passed=True).count(), 1)
 
     def test_invalid(self):
         validator = FilenameValidator()
-        with self.assertRaises(ValidationError):
-            validator.validate('foo?.xml')
+        with tempfile.NamedTemporaryFile(dir=self.datadir, suffix='#.pdf') as f:
+            validator = FilenameValidator()
+            with self.assertRaises(ValidationError):
+                validator.validate(f.name)
+
+        self.assertEqual(Validation.objects.count(), 1)
+        self.assertEqual(Validation.objects.filter(passed=False).count(), 1)
+
+    def test_invalid_directory(self):
+        with tempfile.TemporaryDirectory(dir=self.datadir, suffix='.pdf') as f:
+            validator = FilenameValidator()
+            with self.assertRaises(ValidationError):
+                validator.validate(f)
 
         self.assertEqual(Validation.objects.count(), 1)
         self.assertEqual(Validation.objects.filter(passed=False).count(), 1)
