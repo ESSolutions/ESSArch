dfdaad9be1103f00ee0b8e8fa058769696256e87
Petrus Jonsson <petrus@essolutions.se>
2019-03-18 16:10:52 +0100

    Add first version of build report modal

--- ESSArch_Core/frontend/lang/sv/dashboard.js
+++ ESSArch_Core/frontend/lang/sv/dashboard.js
@@ -3,10 +3,15 @@ angular.module('essarch.language').config(function($translateProvider) {
     DASHBOARD: {
       APPRAISALS: 'Utförda gallringar',
       ARCHIVAL_DESCRIPTION: 'Arkivredovisning',
+      BUILD_REPORT: 'Bygg rapport',
+      GENERATE_REPORT: 'Generera rapport',
+      INFORMATION_PACKAGES: 'Informationspaket',
       ORDERED_INFORMATION_PACKAGES: 'Beställda utlämnandepaket',
       PERMISSIONS: 'Behörigheter',
       ROLES: 'Roller',
+      SELECT_INFORMATION_FOR_REPORT: 'Välj vilken information som ska inlkuderas i rapporten',
       SYSTEM: 'Bestånd',
+      TAGS: 'Arkivredovisning',
       TOTAL_OBJECT_SIZE: 'Total storlek',
       USERS: 'Användare',
       USERS_AND_PERMISSIONS: 'Användare och behörigheter'
