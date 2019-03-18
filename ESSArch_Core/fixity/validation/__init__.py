fd37dd16f31485c541998e730c2a60c491b7102a
Oskar Persson <oskar.persson@polken.se>
2019-03-18 13:26:41 +0100

    Fix validate_path not running validators (#263)

--- ESSArch_Core/fixity/validation/__init__.py
+++ ESSArch_Core/fixity/validation/__init__.py
@@ -54,6 +54,7 @@ def _validate_file(path, validators, task=None, ip=None, stop_at_failure=True, r
 
         try:
             validator.data[PATH_VARIABLE] = path
+            validator.validate(path)
         except Exception:
             if stop_at_failure:
                 raise
@@ -66,6 +67,7 @@ def _validate_directory(path, validators, task=None, ip=None, stop_at_failure=Tr
     for validator in dir_validators:
         try:
             validator.data[PATH_VARIABLE] = path
+            validator.validate(path)
         except Exception:
             if stop_at_failure:
                 raise
@@ -87,9 +89,6 @@ def validate_path(path, validators, profile, data=None, task=None, ip=None, stop
     validator_instances = []
 
     for name in validators:
-        if name not in AVAILABLE_VALIDATORS.keys():
-            raise ValueError('Validator "%s" not specified in profile' % name)
-
         try:
             module_name, validator_class = AVAILABLE_VALIDATORS[name].rsplit('.', 1)
         except KeyError:
