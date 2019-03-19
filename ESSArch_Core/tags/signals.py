21276b2609c4d2602ce4f77c7045f3e479ec619b
haniffm <haniffm@kth.se>
2019-03-19 12:45:40 +0100

    Add logging when TagVersions are created/deleted/modified

--- ESSArch_Core/tags/signals.py
+++ ESSArch_Core/tags/signals.py
@@ -1,9 +1,9 @@
 import logging
 
-from django.db.models.signals import pre_delete, post_save
+from django.db.models.signals import pre_delete, post_save, post_delete
 from django.dispatch import receiver
 
-from ESSArch_Core.tags.models import TagVersion
+from ESSArch_Core.tags.models import TagVersion, Tag
 
 logger = logging.getLogger('essarch.core')
 
@@ -11,13 +11,17 @@ logger = logging.getLogger('essarch.core')
 @receiver(post_save, sender=TagVersion)
 def set_current_version_after_creation(sender, instance, created, **kwargs):
     if created and instance.tag.current_version is None:
+        logger.info(f"TagVersion '{instance}' was created!")
         tag = instance.tag
         tag.current_version = instance
         tag.save(update_fields=['current_version'])
+    else:
+        logger.info(f"TagVersion '{instance}' was updated!")
 
 
 @receiver(pre_delete, sender=TagVersion)
 def fix_current_version_before_version_delete(sender, instance, **kwargs):
+    logger.info(f"Changing current version of TagVersion: '{instance}', before deleting!")
     if instance.tag.current_version == instance:
         try:
             tag = instance.tag
@@ -25,3 +29,13 @@ def fix_current_version_before_version_delete(sender, instance, **kwargs):
             tag.save(update_fields=['current_version'])
         except TagVersion.DoesNotExist:
             pass
+
+
+@receiver(post_delete, sender=TagVersion)
+def log_after_deleting_tag_version(sender, instance, **kwargs):
+    logger.info(f"TagVersion '{instance}' was deleted!")
+
+
+@receiver(post_delete, sender=Tag)
+def log_after_deleting_tag(sender, instance, **kwargs):
+    logger.info(f"Tag '{instance}' was deleted!")
