3372e5acffbe9d353ceccb78d35ad524aa6d5e9d
Oskar Persson <oskar.persson@polken.se>
2019-04-17 08:37:16 +0200

    Add NodeRelationType to admin, add more columns to TagVersion and StructureUnitType admin pages

--- ESSArch_Core/tags/admin.py
+++ ESSArch_Core/tags/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 
 from ESSArch_Core.tags.models import (
+    NodeRelationType,
     StructureType,
     StructureUnitType,
     TagVersionType,
 )
 
 
+class StructureUnitTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'structure_type')
+
+
+class TagVersionTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'archive_type')
+
+
+admin.site.register(NodeRelationType)
 admin.site.register(StructureType)
-admin.site.register(StructureUnitType)
-admin.site.register(TagVersionType)
+admin.site.register(StructureUnitType, StructureUnitTypeAdmin)
+admin.site.register(TagVersionType, TagVersionTypeAdmin)
