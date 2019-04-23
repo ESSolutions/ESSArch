cbc7787fa4c48edb25b06a6359c7872c8234cb3d
Petrus Jonsson <petrus@essolutions.se>
2019-04-23 17:47:23 +0200

    Add options parameter to getDiff function

--- ESSArch_Core/frontend/scripts/services/Utils.js
+++ ESSArch_Core/frontend/scripts/services/Utils.js
@@ -1,15 +1,28 @@
 angular.module('essarch.services').factory('Utils', function() {
   var service = {
-    getDiff: function(obja, objb) {
+    getDiff: function(obja, objb, options) {
       var diff = {};
       for (var key in objb) {
-        if((typeof objb[key] !== 'object') && objb[key] !== obja[key]) {
-          diff[key] = objb[key];
+        if (
+          typeof objb[key] !== 'object' &&
+          objb[key] !== obja[key] &&
+          (angular.isUndefined(options.map[key]) || objb[key] !== obja[key][options.map[key]])
+          ) {
+            diff[key] = objb[key];
         }
       }
       for (var key in obja) {
-        if((typeof obja[key] !== 'object') && obja[key] !== objb[key] && !diff[key]) {
-          diff[key] = null;
+        if (
+          typeof obja[key] !== 'object' &&
+          obja[key] !== objb[key] &&
+          !diff[key] &&
+          (angular.isUndefined(options.map[key]) || obja[key][options.map[key]] !== objb[key])
+          ) {
+          if (diff[key]) {
+            delete diff[key];
+          } else {
+            diff[key] = null;
+          }
         }
       }
       return diff;
