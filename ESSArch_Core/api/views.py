87b0507e3d5288f3af79eab6e5e00a8f259789fc
Oskar Persson <oskar.persson@polken.se>
2019-05-03 16:38:36 +0200

    Update default language order

--- ESSArch_Core/api/views.py
+++ ESSArch_Core/api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Case, IntegerField, Value, When
 from django_filters.rest_framework import DjangoFilterBackend
 from languages_plus.models import Language
 from rest_framework import viewsets
@@ -8,7 +9,13 @@ from ESSArch_Core.auth.permissions import ActionPermissions
 
 
 class LanguageViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Language.objects.all()
+    queryset = Language.objects.all().annotate(
+        default_order=Case(
+            When(iso_639_1='sv', then=Value(1)),
+            When(iso_639_1='en', then=Value(2)),
+            output_field=IntegerField(),
+        )
+    ).order_by('default_order')
     serializer_class = LanguageSerializer
     permission_classes = (ActionPermissions,)
     filter_backends = (DjangoFilterBackend, SearchFilter,)
