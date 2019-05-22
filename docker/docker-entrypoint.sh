f76e9c87e1a677307a38dd14a60c02852294ae72
haniffm <haniffm@kth.se>
2019-05-22 11:53:20 +0200

    Create data folders on start of docker container.
    Also speed up docker creation by ignoring some unnecessary folders/files

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+set -e
+
+echo "Creating data folders"
+mkdir -p /ESSArch/log/ \
+         /ESSArch/etp/env \
+         /ESSArch/data/etp/prepare \
+         /ESSArch/data/etp/prepare_reception \
+         /ESSArch/data/etp/reception \
+         /ESSArch/data/gate/reception \
+         /ESSArch/data/epp/ingest \
+         /ESSArch/data/epp/cache \
+         /ESSArch/data/epp/work \
+         /ESSArch/data/epp/disseminations \
+         /ESSArch/data/epp/orders \
+         /ESSArch/data/epp/verify \
+         /ESSArch/data/epp/temp \
+         /ESSArch/data/epp/reports/appraisal \
+         /ESSArch/data/epp/reports/conversion \
+         /ESSArch/data/eta/reception/eft \
+         /ESSArch/data/eta/uip \
+         /ESSArch/data/eta/work
+
 echo "Apply database migrations"
 python manage.py migrate
 
