0d23b350a365dc639f4216c3366427b5e3a62eb6
haniffm <haniffm@kth.se>
2019-05-24 14:32:21 +0200

    Move ETP/workflow into Core

--- ESSArch_Core/workflow/polling/backends/directory.py
+++ ESSArch_Core/workflow/polling/backends/directory.py
@@ -3,6 +3,7 @@
 import errno
 import logging
 import os
+import shutil
 
 from django.conf import settings
 from lxml import etree
@@ -183,3 +184,59 @@ class DirectoryWorkflowPoller(BaseWorkflowPoller):
             except OSError as e:
                 if e.errno != errno.ENOENT:
                     raise
+
+
+#TODO: these are moved here from ETP and should be merged with above
+logger = logging.getLogger('essarch.etp.workflow.polling.DirectoryWorkflowPoller')
+class DirectoryWorkflowPoller(BaseWorkflowPoller):
+    def poll(self, path, sa=None):
+        for entry in os.listdir(path):
+            subpath = os.path.join(path, entry)
+
+            if os.path.isfile(subpath):
+                continue
+
+            objid = os.path.basename(subpath)
+            if InformationPackage.objects.filter(object_identifier_value=objid).exists():
+                logger.debug(u'Information package with object identifier value "{}" already exists'.format(objid))
+                continue
+
+            if not stable_path(subpath):
+                continue
+
+            sa = SubmissionAgreement.objects.get(name=sa)
+            if sa.profile_workflow is None:
+                logger.debug(u'No workflow profile in SA, skipping')
+                continue
+            if proj not in sa.profile_workflow.specification:
+                logger.debug(
+                    'No workflow specified in {} for current project {}, skipping'.format(
+                        sa.profile_workflow, proj
+                    )
+                )
+                continue
+
+            org = Group.objects.get(name='Default')
+            role = 'admin'
+            responsible = GroupMember.objects.filter(roles__codename=role, group=org).get().member.django_user
+
+            ip = InformationPackage.objects.create(
+                object_identifier_value=objid,
+                object_path=subpath,
+                package_type=InformationPackage.SIP,
+                submission_agreement=sa,
+                submission_agreement_locked=True,
+                state='Prepared',
+                responsible=responsible,
+            )
+            ip.create_profile_rels(p_types, responsible)
+            org.add_object(ip)
+            yield ip
+
+    def delete_source(self, path, ip):
+        path = os.path.join(path, ip.object_identifier_value)
+        try:
+            shutil.rmtree(path)
+        except OSError as e:
+            if e.errno != errno.ENOENT:
+                raise
