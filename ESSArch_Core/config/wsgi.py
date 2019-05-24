afc6c05b9899ac4c6f48ede12fdd5b37e1f5f27c
haniffm <haniffm@kth.se>
2019-05-24 16:33:44 +0200

    Copied asgi.py, wsgi.py, celerydetp, celeryeta and celeryepp into Core

---
+++ ESSArch_Core/config/wsgi.py
@@ -0,0 +1,40 @@
+"""
+    ESSArch is an open source archiving and digital preservation system
+
+    ESSArch
+    Copyright (C) 2005-2017 ES Solutions AB
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+    Contact information:
+    Web - http://www.essolutions.se
+    Email - essarch@essolutions.se
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+"""
+WSGI config for ESSArch project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+application = get_wsgi_application()
