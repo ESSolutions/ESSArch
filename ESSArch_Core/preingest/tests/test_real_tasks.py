6d61c6e5a8bdf240b270c71284f0e3f25ecea4b1
haniffm <haniffm@kth.se>
2019-05-24 13:19:11 +0200

    Move (non-working) preingest/tests from ETP into Core
    Note that the test were not working in ETP repo either

--- ESSArch_Core/preingest/tests/test_real_tasks.py
+++ ESSArch_Core/preingest/tests/test_real_tasks.py
@@ -26,19 +26,20 @@ import os
 import shutil
 
 from django.conf import settings
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, TransactionTestCase
 from django.utils import timezone
 
 from unittest import mock
 
-from install.install_default_config_eta import installDefaultEventTypes
+from ESSArch_Core.install.install_default_config import installDefaultEventTypes
 
 from ESSArch_Core.configuration.models import (
-    Path,
+    EventType, Path,
 )
 
 from ESSArch_Core.ip.models import (
-    InformationPackage,
+    EventIP, InformationPackage,
 )
 
 from ESSArch_Core.WorkflowEngine.models import (
@@ -151,3 +152,182 @@ class test_tasks(TestCase):
 
         self.assertFalse(os.path.isfile(os.path.join(self.gate_reception, str(ip.pk) + ".tar")))
         self.assertFalse(os.path.isfile(os.path.join(self.gate_reception, str(ip.pk) + ".xml")))
+
+
+class test_tasks_etp(TransactionTestCase):
+    def setUp(self):
+        settings.CELERY_ALWAYS_EAGER = True
+        settings.CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
+        self.root = os.path.dirname(os.path.realpath(__file__))
+        self.prepare_path = os.path.join(self.root, "prepare")
+        self.preingest_reception = os.path.join(self.root, "preingest_reception")
+        self.ingest_reception = os.path.join(self.root, "ingest_reception")
+
+        for path in [self.prepare_path, self.preingest_reception, self.ingest_reception]:
+            try:
+                os.makedirs(path)
+            except OSError as e:
+                if e.errno != 17:
+                    raise
+
+        Path.objects.create(
+            entity="path_preingest_prepare",
+            value=self.prepare_path
+        )
+        Path.objects.create(
+            entity="path_preingest_reception",
+            value=self.preingest_reception
+        )
+        Path.objects.create(
+            entity="path_ingest_reception",
+            value=self.ingest_reception
+        )
+
+    def tearDown(self):
+        for path in [self.prepare_path, self.preingest_reception, self.ingest_reception]:
+            try:
+                shutil.rmtree(path)
+            except BaseException:
+                pass
+
+    def test_prepare_ip(self):
+        label = "ip1"
+        user = User.objects.create(username="user1")
+        event_type = EventType.objects.create(eventType=10100)
+
+        task = ProcessTask.objects.create(
+            name="preingest.tasks.PrepareIP",
+            params={
+                "label": label,
+                "responsible": str(user.pk)
+            },
+            responsible=user
+        )
+        task.run()
+
+        ip = InformationPackage.objects.filter(label=label).first()
+
+        self.assertIsNotNone(ip)
+
+        self.assertTrue(
+            EventIP.objects.filter(
+                linkingObjectIdentifierValue=ip,
+                eventOutcome=0,
+                linkingAgentIdentifierValue=user,
+                eventType=event_type,
+            ).exists()
+        )
+
+        task.undo()
+
+        self.assertFalse(
+            InformationPackage.objects.filter(label=label).exists()
+        )
+
+    def test_create_ip_root_dir(self):
+        ip = InformationPackage.objects.create(label="ip1")
+        user = User.objects.create(username="user1")
+        prepare_path = Path.objects.get(entity="path_preingest_prepare").value
+        prepare_path = os.path.join(prepare_path, str(ip.pk))
+        event_type = EventType.objects.create(eventType=10110)
+
+        task = ProcessTask.objects.create(
+            name="preingest.tasks.CreateIPRootDir",
+            params={
+                "information_package": ip.pk,
+            },
+            responsible=user
+        )
+        task.run()
+
+        self.assertTrue(
+            os.path.isdir(prepare_path)
+        )
+
+        self.assertTrue(
+            EventIP.objects.filter(
+                linkingObjectIdentifierValue=ip,
+                eventOutcome=0,
+                linkingAgentIdentifierValue=user,
+                eventType=event_type,
+            ).exists()
+        )
+
+        task.undo()
+
+        self.assertFalse(
+            os.path.isdir(prepare_path)
+        )
+
+    def test_create_physical_model(self):
+        ip = InformationPackage.objects.create(label="ip1")
+        prepare_path = Path.objects.get(entity="path_preingest_prepare").value
+        path = os.path.join(prepare_path, str(ip.pk))
+
+        task = ProcessTask.objects.create(
+            name="preingest.tasks.CreatePhysicalModel",
+            params={
+                "structure": [
+                    {
+                        "name": "dir1",
+                        "type": "folder"
+                    },
+                    {
+                        "name": "dir2",
+                        "type": "folder",
+                    },
+                    {
+                        "name": "file1",
+                        "type": "file"
+                    }
+                ]
+            },
+            information_package=ip,
+        )
+        task.run()
+
+        self.assertTrue(
+            os.path.isdir(os.path.join(path, 'dir1'))
+        )
+        self.assertTrue(
+            os.path.isdir(os.path.join(path, 'dir2'))
+        )
+        self.assertFalse(
+            os.path.isfile(os.path.join(path, 'file1'))
+        )
+
+        task.undo()
+
+        self.assertFalse(
+            os.path.isdir(os.path.join(path, 'dir1'))
+        )
+        self.assertFalse(
+            os.path.isdir(os.path.join(path, 'dir2'))
+        )
+
+    def test_submit_sip(self):
+        ip = InformationPackage.objects.create(label="ip1")
+
+        srctar = os.path.join(self.preingest_reception, "%s.tar" % ip.pk)
+        srcxml = os.path.join(self.preingest_reception, "%s.xml" % ip.pk)
+        dsttar = os.path.join(self.ingest_reception, "%s.tar" % ip.pk)
+        dstxml = os.path.join(self.ingest_reception, "%s.xml" % ip.pk)
+        open(srctar, "a").close()
+        open(srcxml, "a").close()
+
+        task = ProcessTask.objects.create(
+            name="preingest.tasks.SubmitSIP",
+            params={
+                "ip": ip.pk
+            },
+        )
+        task.run()
+
+        self.assertTrue(os.path.isfile(dsttar))
+        self.assertTrue(os.path.isfile(dstxml))
+
+        task.undo()
+
+        self.assertFalse(os.path.isfile(dsttar))
+        self.assertFalse(os.path.isfile(dstxml))
