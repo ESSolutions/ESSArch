4c46c334217f4afbd8a826496e7b5c98c9ce7658
Oskar Persson <oskar.persson@polken.se>
2019-07-05 16:38:03 +0200

    Fix DiskStorageBackend.write storage_method arg

--- ESSArch_Core/storage/backends/disk.py
+++ ESSArch_Core/storage/backends/disk.py
@@ -48,7 +48,7 @@ class DiskStorageBackend(BaseStorageBackend):
         else:
             return copy(src, dst, block_size=block_size)
 
-    def write(self, src, ip, container, storage_medium, block_size=DEFAULT_BLOCK_SIZE):
+    def write(self, src, ip, storage_method, storage_medium, block_size=DEFAULT_BLOCK_SIZE):
         if isinstance(src, str):
             src = [src]
         dst = storage_medium.storage_target.target
