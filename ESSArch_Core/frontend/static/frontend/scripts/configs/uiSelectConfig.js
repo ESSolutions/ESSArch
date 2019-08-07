008a5e9e174d928c6d29b2cdad5550ae57025864
Petrus Jonsson <petrus@essolutions.se>
2019-08-07 10:03:32 +0200

    Convert propsFilter and uiSelectConfig to es6

--- ESSArch_Core/frontend/static/frontend/scripts/configs/uiSelectConfig.js
+++ ESSArch_Core/frontend/static/frontend/scripts/configs/uiSelectConfig.js
@@ -22,44 +22,12 @@
     Email - essarch@essolutions.se
 */
 
-angular
-  .module('essarch.configs')
-  .config(function(uiSelectConfig) {
+const UiSelectConfig = [
+  'uiSelectConfig',
+  function(uiSelectConfig) {
     uiSelectConfig.theme = 'bootstrap';
     uiSelectConfig.appendToBody = true;
-  })
-  .filter('propsFilter', function() {
-    return function(items, props) {
-      var out = [];
+  },
+];
 
-      if (angular.isArray(items)) {
-        items.forEach(function(item) {
-          var itemMatches = false;
-
-          var keys = Object.keys(props);
-          for (var i = 0; i < keys.length; i++) {
-            var prop = keys[i];
-            var text = props[prop].toLowerCase();
-            if (
-              item[prop]
-                .toString()
-                .toLowerCase()
-                .indexOf(text) !== -1
-            ) {
-              itemMatches = true;
-              break;
-            }
-          }
-
-          if (itemMatches) {
-            out.push(item);
-          }
-        });
-      } else {
-        // Let the output be the input untouched
-        out = items;
-      }
-
-      return out;
-    };
-  });
+export default UiSelectConfig;
--- ESSArch_Core/frontend/static/frontend/scripts/modules/essarch.configs.module.js
+++ ESSArch_Core/frontend/static/frontend/scripts/modules/essarch.configs.module.js
@@ -1,3 +1,9 @@
 import HttpInterceptor from '../configs/httpInterceptor';
+import UiSelectConfig from '../configs/uiSelectConfig';
+import PropsFilter from '../filters/propsfilter';
 
-export default angular.module('essarch.configs', ['pascalprecht.translate']).config(HttpInterceptor).name;
+export default angular
+  .module('essarch.configs', ['pascalprecht.translate'])
+  .config(HttpInterceptor)
+  .config(UiSelectConfig)
+  .filter('propsFilter', PropsFilter).name;
