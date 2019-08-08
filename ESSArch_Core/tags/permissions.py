4f23f954de32ced2c4d6f8927adde8acb770f2dd
Oskar Persson <oskar.persson@polken.se>
2019-08-08 11:13:11 +0200

    Remove redundant code

--- ESSArch_Core/tags/permissions.py
+++ ESSArch_Core/tags/permissions.py
@@ -3,7 +3,4 @@ from rest_framework.permissions import BasePermission
 
 class SearchPermissions(BasePermission):
     def has_permission(self, request, view):
-        if request.user.has_perm('tags.search'):
-            return True
-
-        return False
+        return request.user.has_perm('tags.search')
