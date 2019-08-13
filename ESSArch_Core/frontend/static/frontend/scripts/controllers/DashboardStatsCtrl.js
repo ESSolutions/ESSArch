0aad2e56e055665e872b44d663c820f0abe9150e
Petrus Briland <petrus@essolutions.se>
2019-08-13 13:40:54 +0200

    Merge DashboardStatsCtrl

--- ESSArch_Core/frontend/static/frontend/scripts/controllers/DashboardStatsCtrl.js
+++ ESSArch_Core/frontend/static/frontend/scripts/controllers/DashboardStatsCtrl.js
@@ -1,63 +1,55 @@
-/*
-<<<<<<< HEAD:ESSArch_Core/frontend/static/frontend/scripts/controllers/DashboardStatsCtrl.js
 export default class DashboardStatsCtrl {
-  constructor(appConfig, $http, $uibModal, $log) {
-=======
-angular
-  .module('essarch.controllers')
-  .controller('DashboardStatsCtrl', function($scope, appConfig, $http, $uibModal, $log, $translate) {
->>>>>>> origin/tag-agents:ESSArch_Core/frontend/scripts/controllers/DashboardStatsCtrl.js
-*/
-var vm = this;
-vm.$onInit = function() {
-  vm.getStats().then(function(stats) {
-    vm.stats = stats;
-    vm.getAgents();
-  });
-};
+  constructor(appConfig, $http, $uibModal, $log, $translate) {
+    var vm = this;
+    vm.$onInit = function() {
+      vm.getStats().then(function(stats) {
+        vm.stats = stats;
+        vm.getAgents();
+      });
+    };
 
-vm.getAgents = function() {
-  return $http({
-    url: appConfig.djangoUrl + 'agents/',
-    method: 'HEAD',
-    params: {
-      pager: 'none',
-    },
-  }).then(function(response) {
-    vm.stats.tags.unshift({type: $translate.instant('ARCHIVECREATORS'), total: response.headers('Count')});
-    return response.data;
-  });
-};
+    vm.getAgents = function() {
+      return $http({
+        url: appConfig.djangoUrl + 'agents/',
+        method: 'HEAD',
+        params: {
+          pager: 'none',
+        },
+      }).then(function(response) {
+        vm.stats.tags.unshift({type: $translate.instant('ARCHIVECREATORS'), total: response.headers('Count')});
+        return response.data;
+      });
+    };
 
-vm.getStats = function() {
-  return $http.get(appConfig.djangoUrl + 'stats/').then(function(response) {
-    return response.data;
-  });
-};
+    vm.getStats = function() {
+      return $http.get(appConfig.djangoUrl + 'stats/').then(function(response) {
+        return response.data;
+      });
+    };
 
-vm.buildReportModal = function() {
-  var modalInstance = $uibModal.open({
-    animation: true,
-    ariaLabelledBy: 'modal-title',
-    ariaDescribedBy: 'modal-body',
-    templateUrl: 'static/frontend/views/modals/stats_report_modal.html',
-    controller: 'StatsReportModalInstanceCtrl',
-    controllerAs: '$ctrl',
-    size: 'lg',
-    resolve: {
-      data: function() {
-        return {
-          options: vm.stats,
-        };
-      },
-    },
-  });
-  modalInstance.result.then(
-    function(data) {},
-    function() {
-      $log.info('modal-component dismissed at: ' + new Date());
-    }
-  );
-};
-//  }
-//}
+    vm.buildReportModal = function() {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        ariaLabelledBy: 'modal-title',
+        ariaDescribedBy: 'modal-body',
+        templateUrl: 'static/frontend/views/modals/stats_report_modal.html',
+        controller: 'StatsReportModalInstanceCtrl',
+        controllerAs: '$ctrl',
+        size: 'lg',
+        resolve: {
+          data: function() {
+            return {
+              options: vm.stats,
+            };
+          },
+        },
+      });
+      modalInstance.result.then(
+        function(data) {},
+        function() {
+          $log.info('modal-component dismissed at: ' + new Date());
+        }
+      );
+    };
+  }
+}
