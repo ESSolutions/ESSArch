a300a1abc02cc6fd7add231bf21ca158b6aec683
Petrus Briland <petrus@essolutions.se>
2019-08-13 15:23:15 +0200

    Merge api/metadata.py

--- ESSArch_Core/api/metadata.py
+++ ESSArch_Core/api/metadata.py
@@ -3,10 +3,7 @@ from collections import OrderedDict
 from django.utils.encoding import force_text
 from django_filters.utils import label_for_filter
 from rest_framework.metadata import SimpleMetadata
-<<<<<<< HEAD
 from rest_framework.relations import ManyRelatedField, RelatedField
-=======
->>>>>>> origin/tag-agents
 
 from ESSArch_Core.fixity.transformation import AVAILABLE_TRANSFORMERS
 from ESSArch_Core.WorkflowEngine.polling import AVAILABLE_POLLERS
