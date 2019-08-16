ad6e9fac3daacd215fe948877d57aee88226a299
Petrus Briland <petrus@essolutions.se>
2019-08-16 10:43:31 +0200

    Convert formly config to es6

--- ESSArch_Core/frontend/static/frontend/scripts/modules/essarch.configs.module.js
+++ ESSArch_Core/frontend/static/frontend/scripts/modules/essarch.configs.module.js
@@ -1,9 +1,11 @@
+import FormlyConfig from '../configs/formlyConfig';
 import HttpInterceptor from '../configs/httpInterceptor';
 import UiSelectConfig from '../configs/uiSelectConfig';
 import PropsFilter from '../filters/propsFilter';
 
 export default angular
   .module('essarch.configs', ['pascalprecht.translate'])
+  .config(FormlyConfig)
   .config(HttpInterceptor)
   .config(UiSelectConfig)
   .filter('propsFilter', PropsFilter).name;
