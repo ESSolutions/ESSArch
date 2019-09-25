30cec748049810599357db5bffa50ecdffc14b60
Oskar Persson <oskar.persson@polken.se>
2019-09-25 15:09:02 +0200

    Set default ordering for agents and structures

--- ESSArch_Core/tags/views.py
+++ ESSArch_Core/tags/views.py
@@ -208,6 +208,7 @@ class StructureViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     filter_class = StructureFilter
     ordering_fields = ('name', 'create_date', 'version', 'type', 'published_date',)
     search_fields = ('=id', 'name',)
+    ordering = ('-create_date',)
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update', 'metadata']:
@@ -473,7 +474,7 @@ class TransferViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
             request.data.update(parents_query_dict)
         return super().update(request, *args, **kwargs)
 
-    @transaction.atomic()
+    @transaction.atomic
     @permission_required_or_403('tags.change_transfer')
     @action(detail=True, methods=['post'], url_path='add-nodes')
     def add_nodes(self, request, pk=None):
@@ -488,7 +489,7 @@ class TransferViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
 
         return Response()
 
-    @transaction.atomic()
+    @transaction.atomic
     @permission_required_or_403('tags.change_transfer')
     @action(detail=True, methods=['post'], url_path='remove-nodes')
     def remove_nodes(self, request, pk=None):
