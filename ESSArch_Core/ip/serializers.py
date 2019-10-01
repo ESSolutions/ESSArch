b78dd90c94f5cb28069a354a2aa2c594f083e1aa
Oskar Persson <oskar.persson@polken.se>
2019-10-01 11:39:13 +0200

    Inherit InformationPackageSerializer in NestedInformationPackageSerializer

--- ESSArch_Core/ip/serializers.py
+++ ESSArch_Core/ip/serializers.py
@@ -115,7 +115,9 @@ class InformationPackageSerializer(serializers.ModelSerializer):
     package_type_display = serializers.CharField(source='get_package_type_display')
     profiles = serializers.SerializerMethodField()
     workarea = serializers.SerializerMethodField()
-    aic = serializers.PrimaryKeyRelatedField(queryset=InformationPackage.objects.all())
+    aic = serializers.PrimaryKeyRelatedField(
+        queryset=InformationPackage.objects.filter(package_type=InformationPackage.AIC)
+    )
     first_generation = serializers.SerializerMethodField()
     last_generation = serializers.SerializerMethodField()
     organization = serializers.SerializerMethodField()
@@ -459,47 +461,16 @@ class InformationPackageFromMasterSerializer(serializers.ModelSerializer):
         }
 
 
-class NestedInformationPackageSerializer(serializers.ModelSerializer):
-    responsible = UserSerializer(read_only=True)
-    package_type = serializers.ChoiceField(choices=InformationPackage.PACKAGE_TYPE_CHOICES)
-    package_type_display = serializers.SerializerMethodField()
+class NestedInformationPackageSerializer(InformationPackageSerializer):
     information_packages = serializers.SerializerMethodField()
-    aic = serializers.PrimaryKeyRelatedField(queryset=InformationPackage.objects.all())
-    submission_agreement = serializers.PrimaryKeyRelatedField(queryset=SubmissionAgreement.objects.all())
     workarea = serializers.SerializerMethodField()
-    first_generation = serializers.SerializerMethodField()
-    last_generation = serializers.SerializerMethodField()
     new_version_in_progress = serializers.SerializerMethodField()
     permissions = serializers.SerializerMethodField()
-    agents = serializers.SerializerMethodField()
-    organization = serializers.SerializerMethodField()
 
     search_filter = SearchFilter()
 
-    def get_organization(self, obj):
-        try:
-            ctype = ContentType.objects.get_for_model(obj)
-            group = GroupGenericObjects.objects.get(object_id=obj.pk, content_type=ctype).group
-            serializer = GroupSerializer(instance=group)
-            return serializer.data
-        except GroupGenericObjects.DoesNotExist:
-            return None
-
-    def get_package_type_display(self, obj):
-        return obj.get_package_type_display()
-
-    def get_permissions(self, obj):
-        user = getattr(self.context.get('request'), 'user', None)
-        checker = self.context.get('perm_checker')
-        return obj.get_permissions(user=user, checker=checker)
-
-    def get_agents(self, obj):
-        try:
-            agent_objs = obj.prefetched_agents
-        except AttributeError:
-            agent_objs = obj.agents.all()
-        agents = AgentSerializer(agent_objs, many=True).data
-        return {'{role}_{type}'.format(role=a['role'], type=a['type']): a for a in agents}
+    def to_representation(self, obj):
+        return super(serializers.ModelSerializer, self).to_representation(obj)
 
     def get_information_packages(self, obj):
         request = self.context['request']
@@ -509,39 +480,13 @@ class NestedInformationPackageSerializer(serializers.ModelSerializer):
             context={'request': request, 'perm_checker': self.context.get('perm_checker')}
         ).data
 
-    def get_workarea(self, obj):
-        try:
-            workareas = obj.prefetched_workareas
-        except AttributeError:
-            request = self.context.get('request')
-            see_all = request.user.has_perm('ip.see_all_in_workspaces')
-            workareas = obj.workareas.all()
-
-            if not see_all:
-                workareas = workareas.filter(user=request.user)
-
-        return WorkareaSerializer(workareas, many=True, context=self.context).data
-
-    def get_first_generation(self, obj):
-        if hasattr(obj, 'first_generation'):
-            return obj.first_generation
-
-        return obj.is_first_generation()
-
-    def get_last_generation(self, obj):
-        if hasattr(obj, 'last_generation'):
-            return obj.last_generation
-
-        return obj.is_last_generation()
-
     def get_new_version_in_progress(self, obj):
         new = obj.new_version_in_progress()
         if new is None:
             return None
         return WorkareaSerializer(new, context=self.context).data
 
-    class Meta:
-        model = InformationPackage
+    class Meta(InformationPackageSerializer.Meta):
         fields = (
             'id', 'label', 'object_identifier_value', 'package_type', 'package_type_display',
             'responsible', 'create_date', 'entry_date', 'state', 'status',
@@ -553,13 +498,3 @@ class NestedInformationPackageSerializer(serializers.ModelSerializer):
             'new_version_in_progress', 'appraisal_date', 'permissions',
             'organization',
         )
-        extra_kwargs = {
-            'id': {
-                'read_only': False,
-                'validators': [],
-            },
-            'object_identifier_value': {
-                'read_only': False,
-                'validators': [],
-            },
-        }
