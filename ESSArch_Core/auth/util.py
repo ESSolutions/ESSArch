799973c24642b15888d2fdd85067ea33c50a1ce7
Oskar Persson <oskar.persson@polken.se>
2019-10-14 10:55:51 +0200

    Optimize get_objects_for_user

--- ESSArch_Core/auth/util.py
+++ ESSArch_Core/auth/util.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from django.db import connection
-from django.db.models import CharField, F, Min, Q, UUIDField, Value
+from django.db.models import (
+    CharField,
+    Exists,
+    F,
+    Min,
+    OuterRef,
+    Q,
+    UUIDField,
+    Value,
+)
 from django.db.models.functions import Cast, Replace
 from django.shortcuts import _get_queryset
 from guardian.models import GroupObjectPermission, UserObjectPermission
@@ -93,7 +102,7 @@ def get_objects_for_user(user, klass, perms=None, include_no_auth_objs=True):
         codenames.add(codename)
 
     ctype = ContentType.objects.get_for_model(qs.model)
-    role_ids = set()
+    role_ids = GroupGenericObjects.objects.none()
 
     org = user.user_profile.current_organization
     if org is not None:
@@ -102,6 +111,8 @@ def get_objects_for_user(user, klass, perms=None, include_no_auth_objs=True):
         # https://stackoverflow.com/questions/50526873/
         # Fixed in Django 3 (hopefully)
 
+        orgs = []
+
         for org_descendant in org.get_descendants(include_self=True):
             roles = GroupMemberRole.objects.filter(
                 group_memberships__group__in=org_descendant.get_ancestors(include_self=True),
@@ -109,35 +120,69 @@ def get_objects_for_user(user, klass, perms=None, include_no_auth_objs=True):
             )
             role_perms_codenames = set(Permission.objects.filter(roles__in=roles).values_list('codename', flat=True))
             if not len(set(codenames).difference(set(role_perms_codenames))):
-                generic_objects = GroupGenericObjects.objects.filter(content_type=ctype, group=org_descendant)
-                role_ids |= set(generic_objects.values_list('object_id', flat=True))
+                orgs.append(org_descendant)
+
+        role_ids = GroupGenericObjects.objects.filter(content_type=ctype, group__in=orgs)
 
     groups = get_user_groups(user)
-    group_ids = set(GroupObjectPermission.objects.filter(
-        group__essauth_group__in=groups, permission__codename__in=codenames, content_type=ctype
-    ).values_list('object_pk', flat=True))
+    grp_filter_kwargs = {
+        'content_type': ctype,
+    }
+    if len(groups) > 0:
+        grp_filter_kwargs['group__essauth_group__in'] = groups
+    if len(codenames) > 0:
+        grp_filter_kwargs['permission__codename__in'] = codenames
+    else:
+        grp_filter_kwargs['permission__codename__isnull'] = True
+    group_ids = GroupObjectPermission.objects.filter(**grp_filter_kwargs)
 
-    user_ids = set(UserObjectPermission.objects.filter(
-        user=user, permission__codename__in=codenames, content_type=ctype
-    ).values_list('object_pk', flat=True))
+    user_filter_kwargs = {
+        'user': user,
+        'content_type': ctype,
+    }
+    if len(codenames) > 0:
+        user_filter_kwargs['permission__codename__in'] = codenames
+    else:
+        user_filter_kwargs['permission__codename__isnull'] = True
+    user_ids = UserObjectPermission.objects.filter(**user_filter_kwargs)
 
-    all_ids = role_ids | group_ids | user_ids
+    ids_with_no_auth = qs.model.objects.none()
 
     if include_no_auth_objs:
-        ids_with_no_auth = set(qs.annotate(casted_pk=Cast('pk', CharField()))
-                               .exclude(
-                                   casted_pk__in=UserObjectPermission.objects.filter(content_type=ctype).annotate(
-                                       cleaned_pk=replace_func('object_pk', qs.model._meta.pk)
-                                   ).values('cleaned_pk'))
-                               .exclude(
-                                   casted_pk__in=GroupObjectPermission.objects.filter(content_type=ctype).annotate(
-                                       cleaned_pk=replace_func('object_pk', qs.model._meta.pk)
-                                   ).values('cleaned_pk'))
-                               .exclude(
-                                   casted_pk__in=GroupGenericObjects.objects.filter(content_type=ctype).annotate(
-                                       cleaned_pk=replace_func('object_id', qs.model._meta.pk)
-                                   ).values('cleaned_pk'))
-                               .values_list('pk', flat=True))
+        ids_with_no_auth = qs.annotate(casted_pk=Cast('pk', CharField())).exclude(
+            casted_pk__in=UserObjectPermission.objects.filter(content_type=ctype).annotate(
+                cleaned_pk=replace_func('object_pk', qs.model._meta.pk)
+            ).values('cleaned_pk')
+        ).exclude(
+            casted_pk__in=GroupObjectPermission.objects.filter(content_type=ctype).annotate(
+                cleaned_pk=replace_func('object_pk', qs.model._meta.pk)
+            ).values('cleaned_pk')
+        ).exclude(
+            casted_pk__in=GroupGenericObjects.objects.filter(content_type=ctype).annotate(
+                cleaned_pk=replace_func('object_id', qs.model._meta.pk)
+            ).values('cleaned_pk')
+        )
 
-        all_ids |= ids_with_no_auth
-    return qs.filter(pk__in=all_ids)
+    qs = qs.annotate(
+        cleaned_id=replace_func('pk', qs.model._meta.pk),
+        role_exists=Exists(
+            role_ids.annotate(
+                cleaned_pk=replace_func('object_id', qs.model._meta.pk)
+            ).filter(cleaned_pk=OuterRef('pk'))
+        ),
+        grp_exists=Exists(
+            group_ids.annotate(
+                cleaned_pk=replace_func('object_pk', qs.model._meta.pk)
+            ).filter(cleaned_pk=OuterRef('cleaned_id'))
+        ),
+        user_exists=Exists(
+            user_ids.annotate(
+                cleaned_pk=replace_func('object_pk', qs.model._meta.pk)
+            ).filter(cleaned_pk=OuterRef('cleaned_id'))
+        ),
+    ).filter(Q(
+        Q(role_exists=True) |
+        Q(grp_exists=True) |
+        Q(user_exists=True)
+    ))
+    return qs | ids_with_no_auth
