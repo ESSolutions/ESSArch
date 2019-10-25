ESSolutions/ESSArch

commit 9e9837de25ec2318b062a76af5928ab29193190f

Set default permission class for all views to ActionPermissions (#580)

ESSArch_Core/ip/views.py
@@ -205,8 +205,8 @@ class EventIPViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
 
 
 class WorkareaEntryViewSet(mixins.DestroyModelMixin, viewsets.ReadOnlyModelViewSet):
-
     queryset = Workarea.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = WorkareaSerializer
 
     def get_queryset(self):
@@ -346,6 +346,7 @@ class InformationPackageViewSet(viewsets.ModelViewSet):
     logger = logging.getLogger('essarch.InformationPackageViewSet')
 
     queryset = InformationPackage.objects.none()
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = InformationPackageSerializer
     filter_backends = (filters.OrderingFilter, DjangoFilterBackend, SearchFilter,)
     ordering_fields = (
@@ -2657,6 +2658,8 @@ class WorkareaViewSet(InformationPackageViewSet):
 
 
 class WorkareaFilesViewSet(viewsets.ViewSet, PaginatedViewMixin):
+    permission_classes = (permissions.IsAuthenticated,)
+
     def get_user(self, request):
         requested_user = self.request.query_params.get('user')
         if requested_user in EMPTY_VALUES or requested_user == str(request.user.pk):

ESSArch_Core/tags/views.py
@@ -3,7 +3,7 @@ from django.db.models import ProtectedError, Q
 from django.utils.translation import ugettext_lazy as _
 from django_filters.rest_framework import DjangoFilterBackend
 from mptt.templatetags.mptt_tags import cache_tree_children
-from rest_framework import exceptions, filters, viewsets
+from rest_framework import exceptions, filters, permissions, viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
@@ -79,6 +79,7 @@ class ArchiveViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = AgentTagLink.objects.filter(
         tag__elastic_index='archive'
     )
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = AgentArchiveLinkSerializer
     filter_backends = (OrderingFilter, SearchFilter,)
     search_fields = ('tag__name',)

ESSArch_Core/api/tests/test_filters.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django_filters import rest_framework as filters
-from rest_framework import generics, serializers
+from rest_framework import generics, permissions, serializers
 from rest_framework.test import APIRequestFactory, force_authenticate
 
 from ESSArch_Core.api.filters import ListFilter, SearchFilter
@@ -40,6 +40,7 @@ class SearchFilterTests(TestCase):
 
         class SearchListView(generics.ListAPIView):
             queryset = InformationPackage.objects.all()
+            permission_classes = (permissions.IsAuthenticated,)
             serializer_class = SearchFilterSerializer
             filter_backends = (SearchFilter,)
             search_fields = ('id',)

ESSArch_Core/auth/tests/test_decorators.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.test import TestCase
-from rest_framework import status
+from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.test import APIRequestFactory, force_authenticate
 from rest_framework.views import APIView
@@ -16,6 +16,8 @@ class PermissionRequiredOr403Tests(TestCase):
 
     def test_no_permission(self):
         class TestView(APIView):
+            permission_classes = (permissions.IsAuthenticated,)
+
             @permission_required_or_403('ip.add_informationpackage')
             def get(self, request, format=None):
                 return Response({})
@@ -28,6 +30,8 @@ class PermissionRequiredOr403Tests(TestCase):
 
     def test_permission(self):
         class TestView(APIView):
+            permission_classes = (permissions.IsAuthenticated,)
+
             @permission_required_or_403('ip.add_informationpackage')
             def get(self, request, format=None):
                 return Response({})
@@ -42,6 +46,8 @@ class PermissionRequiredOr403Tests(TestCase):
 
     def test_multiple_permissions_granted(self):
         class TestView(APIView):
+            permission_classes = (permissions.IsAuthenticated,)
+
             @permission_required_or_403(['ip.add_informationpackage', 'ip.delete_informationpackage'])
             def get(self, request, format=None):
                 return Response({})
@@ -57,6 +63,8 @@ class PermissionRequiredOr403Tests(TestCase):
 
     def test_multiple_permissions_when_not_all_granted(self):
         class TestView(APIView):
+            permission_classes = (permissions.IsAuthenticated,)
+
             @permission_required_or_403([
                 'ip.add_informationpackage',
                 'ip.delete_informationpackage',
@@ -76,6 +84,8 @@ class PermissionRequiredOr403Tests(TestCase):
 
     def test_multiple_permissions_granted_none_global_perms(self):
         class TestView(APIView):
+            permission_classes = (permissions.IsAuthenticated,)
+
             @permission_required_or_403(
                 ['ip.add_informationpackage', 'ip.delete_informationpackage'],
                 accept_global_perms=False

ESSArch_Core/config/settings.py
@@ -58,7 +58,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'ESSArch_Core.auth.permissions.ActionPermissions',
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }

commit d3f886e3c44ed2422d2c636272df0363f22edad4

Fix and test views broken in #580

ESSArch_Core/config/urls.py
@@ -373,15 +373,19 @@ router.register(r'search', ComponentSearchViewSet, basename='search').register(
 urlpatterns = [
     url(r'^', include('ESSArch_Core.frontend.urls'), name='home'),
     url(r'^admin/', admin.site.urls),
-    url(r'^api/site/', SiteView.as_view()),
-    url(r'^api/stats/$', stats),
-    url(r'^api/stats/export/$', export_stats),
-    url(r'^api/sysinfo/', SysInfoView.as_view()),
+    url(r'^api/site/', SiteView.as_view(), name='configuration-site'),
+    url(r'^api/stats/$', stats, name='stats'),
+    url(r'^api/stats/export/$', export_stats, name='stats-export'),
+    url(r'^api/sysinfo/', SysInfoView.as_view(), name='configuration-sysinfo'),
     url(r'^api/me/$', MeView.as_view(), name='me'),
     url(r'^api/', include(router.urls)),
     url(r'^accounts/changepassword', auth_views.PasswordChangeView.as_view(), {'post_change_redirect': '/'}),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/submission-agreement-template/$', SubmissionAgreementTemplateView.as_view()),
+    url(
+        r'^api/submission-agreement-template/$',
+        SubmissionAgreementTemplateView.as_view(),
+        name='profiles-submission-agreement-template',
+    ),
     url(r'^docs/', include('ESSArch_Core.docs.urls')),
     url(r'^template/', include('ESSArch_Core.essxml.ProfileMaker.urls')),
     url(r'^accounts/login/$', auth_views.LoginView.as_view()),

ESSArch_Core/configuration/views.py
@@ -38,7 +38,7 @@ from django_redis import get_redis_connection
 from elasticsearch.exceptions import ElasticsearchException
 from elasticsearch_dsl.connections import get_connection as get_es_connection
 from redis.exceptions import RedisError
-from rest_framework import filters, viewsets
+from rest_framework import filters, permissions, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -141,6 +141,7 @@ class SysInfoView(APIView):
     """
     API endpoint that allows system info to be viewed
     """
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get(self, request):
         full = string_to_bool(request.query_params.get('full', 'false'))
@@ -236,10 +237,12 @@ class StoragePolicyViewSet(viewsets.ModelViewSet):
 
 
 class SiteView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
     def get(self, request):
         site = Site.objects.first()
         if site is None:
-            return None
+            return Response()
 
         serializer = SiteSerializer(instance=site)
         return Response(serializer.data)

ESSArch_Core/profiles/views.py
@@ -10,7 +10,7 @@ from django.db.models import Max, Prefetch
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import exceptions, serializers, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import SAFE_METHODS
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_extensions.mixins import NestedViewSetMixin
@@ -618,5 +618,7 @@ class ProfileMakerTemplateViewSet(viewsets.ModelViewSet):
 
 
 class SubmissionAgreementTemplateView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         return Response(get_sa_template())

ESSArch_Core/stats/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import Permission
 from django.db.models import Count, Sum
 from django.template.loader import render_to_string
 from django.utils import timezone
-from rest_framework import exceptions
-from rest_framework.decorators import api_view
+from rest_framework import exceptions, permissions
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from weasyprint import HTML
 
@@ -34,11 +34,13 @@ def get_data():
 
 
 @api_view()
+@permission_classes((permissions.IsAuthenticated,))
 def stats(request):
     return Response(get_data())
 
 
 @api_view()
+@permission_classes((permissions.IsAuthenticated,))
 def export(request):
     data = get_data()
 

ESSArch_Core/configuration/tests/test_views.py
@@ -1,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+User = get_user_model()
+
+
+class SiteTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='user')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        self.url = reverse('configuration-site')
+
+    def test_site(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class SysInfoTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='user')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        self.url = reverse('configuration-sysinfo')
+
+    def test_site(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

ESSArch_Core/profiles/tests/test_views.py
@@ -110,3 +110,15 @@ class CreateSubmissionAgreementTests(TestCase):
         self.client.force_authenticate(user=self.user)
         response = self.client.post(self.url, {'name': 'foo', 'label': 'Foo', 'type': 'sa', 'status': 'created'})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class SubmissionAgreementTemplateTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create(username='user')
+        self.url = reverse('profiles-submission-agreement-template')
+        self.client.force_authenticate(user=self.user)
+
+    def test_authenticated_with_permission(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

ESSArch_Core/stats/tests/test_views.py
@@ -1,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+User = get_user_model()
+
+
+class StatsTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='user')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_list(self):
+        url = reverse('stats')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_export(self):
+        url = reverse('stats-export')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)