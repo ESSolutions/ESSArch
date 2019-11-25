0fd1af2e7129ffd843dad9871fbc99630b0fcb69
Oskar Persson <oskar.persson@polken.se>
2019-11-25 14:43:39 +0100

    Add policy filter to IP API

--- ESSArch_Core/ip/filters.py
+++ ESSArch_Core/ip/filters.py
@@ -59,7 +59,7 @@ class InformationPackageFilter(filters.FilterSet):
     class Meta:
         model = InformationPackage
         fields = ['archivist_organization', 'state', 'responsible', 'active', 'label',
-                  'create_date', 'entry_date', 'object_size', 'start_date', 'end_date',
+                  'create_date', 'entry_date', 'object_size', 'start_date', 'end_date', 'policy',
                   'archived', 'cached', 'package_type', 'package_type_name_exclude', 'workarea']
 
 
