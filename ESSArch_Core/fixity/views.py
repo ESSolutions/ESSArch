428f9b16fb432e54b69bd60a0bb7176059b3c5ef
Oskar Persson <oskar.persson@polken.se>
2019-12-16 15:56:01 +0100

    Use empty list as standard form for validators

--- ESSArch_Core/fixity/views.py
+++ ESSArch_Core/fixity/views.py
@@ -42,7 +42,7 @@ class ValidatorViewSet(viewsets.ViewSet):
             try:
                 form = klass.form()
             except AttributeError:
-                form = {}
+                form = []
 
             validator = {
                 'label': label,
