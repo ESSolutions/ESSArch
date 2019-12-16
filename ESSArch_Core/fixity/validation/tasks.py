e864aa8833453447f72d01fe88eb66004e2e1883
Oskar Persson <oskar.persson@polken.se>
2019-12-16 23:59:44 +0100

    Remove old validation objects when retrying Validate task

--- ESSArch_Core/fixity/validation/tasks.py
+++ ESSArch_Core/fixity/validation/tasks.py
@@ -1,9 +1,13 @@
+from ESSArch_Core.fixity.models import Validation
 from ESSArch_Core.fixity.validation import get_backend
 from ESSArch_Core.WorkflowEngine.dbtask import DBTask
 
 
 class Validate(DBTask):
     def run(self, name, path, context=None, options=None):
+        # delete validations from previous attempts
+        Validation.objects.filter(task=self.get_processtask()).delete()
+
         options = {} if options is None else options
         klass = get_backend(name)
 
