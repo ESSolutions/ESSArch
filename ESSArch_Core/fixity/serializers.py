9cf350482bc61d72612205aeb06175a9f7bc96ad
Oskar Persson <oskar.persson@polken.se>
2019-12-16 15:39:36 +0100

    Add information_package field to ValidatorWorkflowSerializer

--- ESSArch_Core/fixity/serializers.py
+++ ESSArch_Core/fixity/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from ESSArch_Core.fixity.models import ConversionTool, Validation
 from ESSArch_Core.fixity.validation import AVAILABLE_VALIDATORS
+from ESSArch_Core.ip.models import InformationPackage
 
 
 class ConversionToolSerializer(serializers.ModelSerializer):
@@ -18,8 +19,9 @@ class ValidatorDataSerializer(serializers.Serializer):
 
 
 class ValidatorWorkflowSerializer(serializers.Serializer):
+    information_package = serializers.PrimaryKeyRelatedField(queryset=InformationPackage.objects.all())
     validators = serializers.ListField(
-        child=ValidatorDataSerializer()
+        child=ValidatorDataSerializer(),
     )
 
 
