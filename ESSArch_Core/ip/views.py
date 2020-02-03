49aa22a60baf9a59bf9d7610dccc04314517b0f7
Oskar Persson <oskar.persson@polken.se>
2020-02-03 17:31:34 +0100

    Move aic creation to receive

--- ESSArch_Core/ip/views.py
+++ ESSArch_Core/ip/views.py
@@ -1332,14 +1332,6 @@ class InformationPackageViewSet(viewsets.ModelViewSet):
                 profile_ip.LockedBy = request.user
                 profile_ip.save()
 
-            ip.aic = InformationPackage.objects.create(
-                package_type=InformationPackage.AIC,
-                responsible=ip.responsible,
-                label=ip.label,
-                start_date=ip.start_date,
-                end_date=ip.end_date,
-            )
-            ip.generation = 0
             ip.save()
 
             generate_premis = ip.profile_locked('preservation_metadata')
@@ -2318,6 +2310,13 @@ class InformationPackageReceptionViewSet(viewsets.ViewSet, PaginatedViewMixin):
         ip.sip_path = pk
         ip.package_type = InformationPackage.AIP
         ip.generation = 0
+        ip.aic = InformationPackage.objects.create(
+            package_type=InformationPackage.AIC,
+            responsible=ip.responsible,
+            label=ip.label,
+            start_date=ip.start_date,
+            end_date=ip.end_date,
+        )
         ip.state = 'Receiving'
         ip.object_path = normalize_path(container)
         ip.package_mets_path = normalize_path(xmlfile)
