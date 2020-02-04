d00c538b4d57cd76a7b32c37b0ec187f62ff3e88
Oskar Persson <oskar.persson@polken.se>
2020-02-04 12:12:54 +0100

    Skip migration tests on MySQL and MariaDB

--- ESSArch_Core/profiles/tests/test_migrations.py
+++ ESSArch_Core/profiles/tests/test_migrations.py
@@ -1,6 +1,10 @@
+import unittest
+
+from django.db import connection
 from django_test_migrations.contrib.unittest_case import MigratorTestCase
 
 
+@unittest.skipIf(connection.vendor == 'mysql', 'migration tests are not stable on MySQL and MariaDB')
 class PolicyMoveFromInformationPackageToSubmissionAgreement(MigratorTestCase):
     migrate_from = ('profiles', '0053_submissionagreement_policy')
     migrate_to = ('profiles', '0055_non_nullable_sa_policy')
