c677111c8422112285ec7cd62576b9264d8338f6
Oskar Persson <oskar.persson@polken.se>
2020-02-13 10:37:19 +0100

    Add frontend test for login (#967)

---
+++ ESSArch_Core/frontend/tests/__init__.py
@@ -0,0 +1,19 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+class FrontendTestCase(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        options = Options()
+        options.headless = True
+        cls.selenium = WebDriver(options=options)
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
