5a4a959cc1b73970fb607249b74f6fd41c5f0f06
Henrik Ek <henrik@essolutions.se>
2020-04-07 18:40:18 +0200

    Add push notification when conversion job is done

--- ESSArch_Core/fixity/conversion/tasks.py
+++ ESSArch_Core/fixity/conversion/tasks.py
@@ -1,13 +1,18 @@
 import os
+import logging
 from pathlib import PurePath
 
 from glob2 import iglob
+
+from django.contrib.auth import get_user_model
 
 from ESSArch_Core.fixity.models import ConversionTool
 from ESSArch_Core.ip.models import EventIP
 from ESSArch_Core.util import in_directory
 from ESSArch_Core.WorkflowEngine.dbtask import DBTask
+from ESSArch_Core.auth.models import Notification
 
+User = get_user_model()
 
 class Convert(DBTask):
     def _convert(self, path, rootdir, tool, options, delete_original=True):
@@ -19,12 +24,24 @@ class Convert(DBTask):
             eventOutcome=EventIP.SUCCESS,
             eventOutcomeDetailNote='Converted {}'.format(relpath),
             linkingObjectIdentifierValue=str(self.get_information_package().pk),
+            linkingAgentIdentifierValue=User.objects.get(pk=self.responsible)
         )
 
         if delete_original:
             os.remove(path)
 
+    def create_notification(self, ip):
+        Notification.objects.create(
+            message='"{ip}" was successfully converted'.format(
+                ip=ip.object_identifier_value
+            ),
+            level=logging.INFO,
+            user_id=self.responsible,
+            refresh=True
+        )
+
     def run(self, tool, pattern, rootdir, options, delete_original=True):
+        ip = self.get_information_package()
         tool = ConversionTool.objects.get(name=tool)
 
         for path in iglob(rootdir + '/' + pattern, case_sensitive=False):
@@ -38,3 +55,5 @@ class Convert(DBTask):
                         self._convert(fpath, rootdir, tool, options)
             else:
                 self._convert(path, rootdir, tool, options)
+
+        self.create_notification(ip)
