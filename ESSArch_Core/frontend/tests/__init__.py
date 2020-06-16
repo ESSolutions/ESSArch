5cfcf0158c2cf93bcbe4db4af8f6a4670987d814
Henrik Ek <henrik@essolutions.se>
2020-06-16 18:08:46 +0200

    Add try to catch TimeoutException and retry

--- ESSArch_Core/frontend/tests/__init__.py
+++ ESSArch_Core/frontend/tests/__init__.py
@@ -1,4 +1,7 @@
+import time
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.webdriver import WebDriver
 
@@ -10,8 +13,23 @@ class FrontendTestCase(StaticLiveServerTestCase):
 
         options = Options()
         options.headless = True
-        print('setUpClass - WebDriver', flush=True)
-        cls.selenium = WebDriver(options=options)
+        try:
+            print('setUpClass - WebDriver', flush=True)
+            cls.selenium = WebDriver(options=options)
+        except TimeoutException as e:
+            print('setUpClass - TimeoutException - error: %s' % e, flush=True)
+            print('sleep 60', flush=True)
+            time.sleep(60)
+            try:
+                print('setUpClass - WebDriver', flush=True)
+                cls.selenium = WebDriver(options=options)
+            except TimeoutException as e:
+                print('setUpClass - TimeoutException - error: %s' % e, flush=True)
+                print('sleep 60', flush=True)
+                time.sleep(60)
+                print('setUpClass - WebDriver', flush=True)
+                cls.selenium = WebDriver(options=options)
+
         print('setUpClass - implicitly_wait', flush=True)
         cls.selenium.implicitly_wait(10)
 
