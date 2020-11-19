75cd15b2783ecfa33d7c797f8bec7221225bf6fc
henrikek <henrik@essolutions.se>
2020-11-19 23:52:18 +0100

    Move AF1 A-B to ESSArch_Plugins

--- ESSArch_Core/search/importers/__init__.py
+++ ESSArch_Core/search/importers/__init__.py
@@ -5,8 +5,6 @@ from django.conf import settings
 AVAILABLE_IMPORTERS = {
     'eard_erms': 'ESSArch_Core.search.importers.earderms.EardErmsImporter',
     'single_records': 'ESSArch_Core.search.importers.single_records.SingleRecordsImporter',
-    'AF1_fall_A': 'ESSArch_Core.search.importers.AF1_fall_A.AF1_fall_AImporter',
-    'AF1_fall_B': 'ESSArch_Core.search.importers.AF1_fall_B.AF1_fall_BImporter',
 }
 
 extra_importers = getattr(settings, 'ESSARCH_IMPORTERS', {})
