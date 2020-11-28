f8f907ccae2931d6d1592fdc3aa4e3f7748c4fdd
arch <andreassegerberg@gmail.com>
2020-11-28 20:44:22 +0100

    added missing ,

--- ESSArch_Core/frontend/static/frontend/lang/sv/dashboard.ts
+++ ESSArch_Core/frontend/static/frontend/lang/sv/dashboard.ts
@@ -4,7 +4,7 @@ export default ($translateProvider: ng.translate.ITranslateProvider) => {
     DASHBOARD: {
       APPRAISALS: 'Utförda gallringar',
       ARCHIVAL_DESCRIPTION: 'Arkivredovisning',
-      AUTHORITY_RECORDS: 'Arkivbildare'
+      AUTHORITY_RECORDS: 'Arkivbildare',
       BUILD_REPORT: 'Bygg rapport',
       DELIVERIES: 'Registrerade leveranser',
       GENERATE_REPORT: 'Generera rapport',
