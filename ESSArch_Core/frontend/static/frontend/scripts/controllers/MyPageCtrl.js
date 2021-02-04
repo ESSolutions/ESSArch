be03ff3017ef61cf11e20c4fd0fdeb1206b5deff
Nikolaj Mansson <nikolaj.mansson@essolutions.se>
2021-02-04 20:05:53 +0100

    Issue 79: REST API connection to page title and text

--- ESSArch_Core/frontend/static/frontend/scripts/controllers/MyPageCtrl.js
+++ ESSArch_Core/frontend/static/frontend/scripts/controllers/MyPageCtrl.js
@@ -1,5 +1,23 @@
 export default class MyPageCtrl {
-  constructor($scope) {
+  constructor($scope, appConfig, $http) {
     const vm = this;
-  }
+    vm.sites = null;
+
+    vm.$onInit = function () {
+      vm.sitesLoading = true;
+      vm.getSites()
+        .then(function (sites) {
+            vm.sites = sites;
+            vm.sitesLoading = false;
+        })
+        .catch(() => {
+          vm.sitesLoading = false;
+        });
+    };
+    vm.getSites = function () {
+      return $http.get(appConfig.djangoUrl + 'site/').then(function (response) {
+        return response.data;
+      });
+    };
+  }  
 }
