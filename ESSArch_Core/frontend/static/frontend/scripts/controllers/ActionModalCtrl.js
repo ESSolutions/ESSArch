c49dbc0f880450abb7e1d4e55d18690df85e3521
Nikolaj Mansson <nikolaj.mansson@essolutions.se>
2021-02-10 16:47:21 +0100

    Issue 88: Prettier

--- ESSArch_Core/frontend/static/frontend/scripts/controllers/ActionModalCtrl.js
+++ ESSArch_Core/frontend/static/frontend/scripts/controllers/ActionModalCtrl.js
@@ -1,57 +1,56 @@
 import moment from 'moment';
 
 export default class ActionModalCtrl {
-    constructor($scope, $rootScope, $uibModalInstance, currentStepTask) {
-        if (currentStepTask.time_started !== null && currentStepTask.time_done !== null) {
-            const started = moment(currentStepTask.time_started);
-            const done = moment(currentStepTask.time_done);
-            currentStepTask.duration = moment.utc(done.diff(started)).format('HH:mm:ss.SSS');
-          } else {
-            currentStepTask.duration = null;
-          }
-
-          $scope.mapStepStateProgress = $rootScope.mapStepStateProgress;
-        if (currentStepTask.time_started !== null && currentStepTask.time_done !== null) {
-            const started = moment(currentStepTask.time_started);
-            const done = moment(currentStepTask.time_done);
-            currentStepTask.duration = moment.utc(done.diff(started)).format('HH:mm:ss.SSS');
-          } else {
-            currentStepTask.duration = null;
-          }
-
-        $scope.currentStepTask = currentStepTask;
-        $scope.ok = () => {
-            $uibModalInstance.close('remove');
-        };
-        $scope.idCopied = false;
-        $scope.idCopyDone = function () {
-            $scope.idCopied = true;
-          };
-
-        $scope.mapStepStateProgress = $rootScope.mapStepStateProgress;
-        if (currentStepTask.time_started !== null && currentStepTask.time_done !== null) {
-            const started = moment(currentStepTask.time_started);
-            const done = moment(currentStepTask.time_done);
-            currentStepTask.duration = moment.utc(done.diff(started)).format('HH:mm:ss.SSS');
-          } else {
-            currentStepTask.duration = null;
-          }
-
-          $scope.getArgsString = function (args) {
-            if (!angular.isUndefined(args)) {
-              return args
-                .map(function (x) {
-                  if (x === null) {
-                    return 'null';
-                  } else {
-                    return x;
-                  }
-                })
-                .join(', ');
-            } else {
-              return '';
-            }
-          };
-          
+  constructor($scope, $rootScope, $uibModalInstance, currentStepTask) {
+    if (currentStepTask.time_started !== null && currentStepTask.time_done !== null) {
+      const started = moment(currentStepTask.time_started);
+      const done = moment(currentStepTask.time_done);
+      currentStepTask.duration = moment.utc(done.diff(started)).format('HH:mm:ss.SSS');
+    } else {
+      currentStepTask.duration = null;
     }
+
+    $scope.mapStepStateProgress = $rootScope.mapStepStateProgress;
+    if (currentStepTask.time_started !== null && currentStepTask.time_done !== null) {
+      const started = moment(currentStepTask.time_started);
+      const done = moment(currentStepTask.time_done);
+      currentStepTask.duration = moment.utc(done.diff(started)).format('HH:mm:ss.SSS');
+    } else {
+      currentStepTask.duration = null;
+    }
+
+    $scope.currentStepTask = currentStepTask;
+    $scope.ok = () => {
+      $uibModalInstance.close('remove');
+    };
+    $scope.idCopied = false;
+    $scope.idCopyDone = function () {
+      $scope.idCopied = true;
+    };
+
+    $scope.mapStepStateProgress = $rootScope.mapStepStateProgress;
+    if (currentStepTask.time_started !== null && currentStepTask.time_done !== null) {
+      const started = moment(currentStepTask.time_started);
+      const done = moment(currentStepTask.time_done);
+      currentStepTask.duration = moment.utc(done.diff(started)).format('HH:mm:ss.SSS');
+    } else {
+      currentStepTask.duration = null;
+    }
+
+    $scope.getArgsString = function (args) {
+      if (!angular.isUndefined(args)) {
+        return args
+          .map(function (x) {
+            if (x === null) {
+              return 'null';
+            } else {
+              return x;
+            }
+          })
+          .join(', ');
+      } else {
+        return '';
+      }
+    };
   }
+}
