7c383d8992f4e4d233ef824134506c028c80c65c
segerberg <andreassegerberg@gmail.com>
2021-05-28 13:00:44 +0200

    Setting for OpenShift

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,7 +10,10 @@ python ESSArch_Core/install/install_profiles_se.py
 python ESSArch_Core/install/install_profiles_no.py
 python ESSArch_Core/install/install_profiles_eark.py
 
+echo "Starting worker"
+essarch worker -l info -Q celery,file_operation,validation
+
 echo "Starting server"
 python manage.py runserver 0:8000
-essarch worker -l info -Q celery,file_operation,validation
+
 
