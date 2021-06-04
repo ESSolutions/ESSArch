bbe70e60a72d73e2d708b0cddd92d2a5ba219f53
segerberg <andreassegerberg@gmail.com>
2021-06-04 17:38:01 +0200

    revert

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,8 +10,6 @@ python ESSArch_Core/install/install_profiles_se.py
 python ESSArch_Core/install/install_profiles_no.py
 python ESSArch_Core/install/install_profiles_eark.py
 
-echo "Starting worker"
-essarch worker -l info -Q celery,file_operation,validation
 
 echo "Starting server"
 python manage.py runserver 0:8000
