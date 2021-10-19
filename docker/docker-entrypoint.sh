2266ab3e6316fc49efc12bc433893c182ae22388
henrikek <henrik@essolutions.se>
2021-10-19 18:16:31 +0200

    Add nationality config during profile installation

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,9 +6,9 @@ essarch settings generate --debug -q --no-overwrite
 essarch install -q
 
 echo "Installing profiles"
-python ESSArch_Core/install/install_profiles_se.py
-python ESSArch_Core/install/install_profiles_no.py
-python ESSArch_Core/install/install_profiles_eark.py
+python ESSArch_Core/install/install_sa_profiles.py se
+python ESSArch_Core/install/install_sa_profiles.py no
+python ESSArch_Core/install/install_sa_profiles.py eark
 
 echo "Starting server"
 python manage.py runserver 0:8000
