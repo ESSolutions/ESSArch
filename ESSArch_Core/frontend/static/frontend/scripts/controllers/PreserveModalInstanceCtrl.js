73953fa7d35e82f3eddd56090c9df31f6d61d304
arch <andreassegerberg@gmail.com>
2022-09-14 22:49:40 +0200

    lint prettier

--- ESSArch_Core/frontend/static/frontend/scripts/controllers/PreserveModalInstanceCtrl.js
+++ ESSArch_Core/frontend/static/frontend/scripts/controllers/PreserveModalInstanceCtrl.js
@@ -11,34 +11,34 @@ export default class PreserveModalInstanceCtrl {
     vm.request = data.request;
     vm.preserving = false;
 
-    vm.$onInit = function() {
+    vm.$onInit = function () {
       if (!vm.data.ips) {
         vm.data.ips = [vm.data.ip];
       }
       if (isEnabled($rootScope.features, 'archival descriptions'))
-        $scope.getArchives().then(function(result) {
+        $scope.getArchives().then(function (result) {
           vm.tags.archive.options = result;
         });
     };
 
-    $scope.updateTags = function() {
+    $scope.updateTags = function () {
       $scope.tagsLoading = true;
-      $scope.getArchives().then(result => {
+      $scope.getArchives().then((result) => {
         vm.tags.archive.options = result;
         $scope.requestForm = true;
         $scope.tagsLoading = false;
       });
     };
 
-    vm.ok = function() {
+    vm.ok = function () {
       $uibModalInstance.close();
     };
-    vm.cancel = function() {
+    vm.cancel = function () {
       $uibModalInstance.dismiss('cancel');
     };
 
     // Preserve IP
-    vm.preserve = function() {
+    vm.preserve = function () {
       vm.preserving = true;
       let data = {
         purpose: vm.request.purpose,
@@ -49,18 +49,18 @@ export default class PreserveModalInstanceCtrl {
         data.structure_unit = vm.tags.structureUnits.value.id;
       }
       const promises = [];
-      vm.data.ips.forEach(function(ip) {
+      vm.data.ips.forEach(function (ip) {
         promises.push(
           Requests.preserve(ip, data)
-            .then(function(result) {
+            .then(function (result) {
               return result;
             })
-            .catch(function(response) {
+            .catch(function (response) {
               return response;
             })
         );
       });
-      $q.all(promises).then(function(data) {
+      $q.all(promises).then(function (data) {
         $uibModalInstance.close(data);
         vm.preserving = false;
       });
