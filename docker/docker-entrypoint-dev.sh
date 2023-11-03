1dbf98ab32e783dd0d536e1e1c058d4a56da2540
henrikek <henrik@essolutions.se>
2023-11-03 19:56:23 +0100

    Add schemaserver and ssh to docker

--- docker/docker-entrypoint-dev.sh
+++ docker/docker-entrypoint-dev.sh
@@ -15,8 +15,8 @@ python ESSArch_Core/install/install_sa_profiles.py eark
 if [ ! -f .vscode/settings.json ]; then
     cp .vscode/settings.json.default .vscode/settings.json
 fi
-if [ ! -f .vscode/extensionsjson ]; then
-    cp .vscode/extensions.json.default .vscode/extensionsjson
+if [ ! -f .vscode/extensions.json ]; then
+    cp .vscode/extensions.json.default .vscode/extensions.json
 fi
 
 echo "Starting server"
