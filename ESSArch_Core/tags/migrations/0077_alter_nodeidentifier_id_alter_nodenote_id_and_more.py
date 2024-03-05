022f1732fc160742cf5198c8dd191eb81e8e7169
henrikek <henrik@essolutions.se>
2024-03-06 00:22:10 +0100

    Update backend deps 20240306

--- ESSArch_Core/tags/migrations/0077_alter_nodeidentifier_id_alter_nodenote_id_and_more.py
+++ ESSArch_Core/tags/migrations/0077_alter_nodeidentifier_id_alter_nodenote_id_and_more.py
@@ -14,16 +14,17 @@ class AlterField_char32_to_realUUID(AlterField):
         if self.allow_migrate_model(schema_editor.connection.alias, to_model):
             from_model = from_state.apps.get_model(app_label, self.model_name)
             from_field = from_model._meta.get_field(self.name)
-            cursor = schema_editor.connection.cursor()
-            cursor.execute(
-                'SELECT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME=%s and COLUMN_NAME=%s',
-                [from_model._meta.db_table, self.name])
-            from_field_is_char32 = 'char(32)' in cursor.fetchone()
-            mariadb_has_native_uuid_field = schema_editor.connection.features.has_native_uuid_field and \
-                schema_editor.connection.mysql_is_mariadb
-            if from_field_is_char32 and mariadb_has_native_uuid_field:
-                from_field.__class__ = models.fields.CharField
-                from_field.db_collation = None
+            if schema_editor.connection.mysql_is_mariadb:
+                cursor = schema_editor.connection.cursor()
+                cursor.execute(
+                    'SELECT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME=%s and COLUMN_NAME=%s',
+                    [from_model._meta.db_table, self.name])
+                from_field_is_char32 = 'char(32)' in cursor.fetchone()
+                mariadb_has_native_uuid_field = schema_editor.connection.features.has_native_uuid_field and \
+                    schema_editor.connection.mysql_is_mariadb
+                if from_field_is_char32 and mariadb_has_native_uuid_field:
+                    from_field.__class__ = models.fields.CharField
+                    from_field.db_collation = None
             to_field = to_model._meta.get_field(self.name)
             if not self.preserve_default:
                 to_field.default = self.field.default
