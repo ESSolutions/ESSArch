26fcc656fe4aa7c323b381c6af0116cbe281a12a
Henrik Ek <henrik@essolutions.se>
2025-12-02 00:18:45 +0100

    Fix builds of css and js (#2430)

--- webpack.prod.babel.js
+++ webpack.prod.babel.js
@@ -1,7 +1,6 @@
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common.babel.js');
 const path = require('path');
-
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -10,42 +9,23 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 const basedir = path.resolve(__dirname, 'ESSArch_Core/frontend/static/frontend');
 
-module.exports = (env, argv) => {
-  return merge(common(env, argv), {
+module.exports = (env, argv) =>
+  merge(common(env, argv), {
     mode: 'production',
     devtool: 'source-map',
-    output: {
-      filename: '[name]-[chunkhash].js',
-      path: path.resolve(basedir, 'build'),
-    },
+    output: {filename: '[name]-[chunkhash].js', path: path.resolve(basedir, 'build')},
     optimization: {
       minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-            },
-          },
-        }),
-        new CssMinimizerPlugin({
-          minimizerOptions: {
-            preset: [
-              'default',
-              {
-                discardComments: {removeAll: true},
-              },
-            ],
-          },
-        }),
+        new TerserPlugin({terserOptions: {compress: {drop_console: true}}}),
+        new CssMinimizerPlugin({minimizerOptions: {preset: ['default', {discardComments: {removeAll: true}}]}}),
       ],
     },
     plugins: [
       new ForkTsCheckerWebpackPlugin(),
       new MiniCssExtractPlugin({
-        filename: '[name]-[chunkhash].css',
+        filename: '[name]-[contenthash].css',
         chunkFilename: '[id]-[chunkhash].css',
       }),
       new CleanWebpackPlugin(),
     ],
   });
-};
