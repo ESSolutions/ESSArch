be637cdac506e59f346417ee33d39d29e28556f2
henrikek <henrik@essolutions.se>
2025-12-14 14:57:39 +0100

    Load upload config from settings

--- ESSArch_Core/tus/urls.py
+++ ESSArch_Core/tus/urls.py
@@ -1,8 +1,9 @@
-from django.urls import re_path
+from django.urls import path, re_path
 
-from .views import tus_create_view, tus_upload_view
+from .views import tus_create_view, tus_upload_view, upload_config_view
 
 urlpatterns = [
-    re_path(r"^$", tus_create_view, name="tus-create"),
+    path("upload-config/", upload_config_view, name="upload-config"),
+    path("", tus_create_view, name="tus-create"),
     re_path(r"^(?P<upload_id>[^/]+)/?$", tus_upload_view, name="tus-upload"),
 ]
