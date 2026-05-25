12a18cca811e785094942d1bae6fbbffd9026ae0
henrikek <henrik@essolutions.se>
2026-05-25 23:52:05 +0200

    try4

--- ESSArch_Core/testing/runner.py
+++ ESSArch_Core/testing/runner.py
@@ -15,18 +15,36 @@ class ESSArchTestRunner(DiscoverRunner):
         return super().run_tests(*args, **kwargs)
 
     def setup_databases(self, **kwargs):
-        from django.conf import settings
-
-        # IMPORTANT: set BEFORE super()
-        if connection.vendor == "microsoft":
-            settings.MIGRATION_MODULES = {
-                "token_blacklist": None,
-            }
         dbs = super().setup_databases(**kwargs)
         if connection.vendor == 'microsoft':
+            self._handle_mssql_migrations()
             db_name = connection.settings_dict['NAME']
             connection.creation.install_regex_clr(db_name)
         return dbs
+
+    def _handle_mssql_migrations(self):
+        """
+        Automatically fix known MSSQL migration issues.
+        Runs AFTER DB creation but BEFORE tests execute fully.
+        """
+        from django.core.management import call_command
+
+        # 1. Ensure token_blacklist is at safe migration state
+        # (0008 is the problematic one on MSSQL)
+        try:
+            call_command(
+                "migrate",
+                "token_blacklist",
+                "0007",
+                fake=True,
+                verbosity=0,
+            )
+        except Exception:
+            # If already applied or partially applied, ignore
+            pass
+
+        # 2. Continue normal migration process
+        call_command("migrate", verbosity=0)
 
 
 @contextmanager
